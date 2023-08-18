2953054db16851f26bae5c1823a9d98ce2811d18
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-08-18 11:11:48 +0700

    ADD NEW | Unit Testing for update role

--- backend/sources/test/01-role/role.test.js
+++ backend/sources/test/01-role/role.test.js
@@ -115,3 +115,73 @@ describe('POST /', () => {
     });
 });
 
+describe('PUT /role/:id', () => {
+    // Membuat data dummy dengan status active
+    beforeEach(async () => {
+        await connection.roles.createMany({
+            data: [
+                {
+                    ROLE_ID: `dummy-role`,
+                    ROLE_NAME: `dummy-role`,
+                    ROLE_IS_DELETED: false
+                },
+                {
+                    ROLE_ID: `dummy-role-2`,
+                    ROLE_NAME: `dummy-role-2`,
+                    ROLE_IS_DELETED: true
+                }
+            ], skipDuplicates: true
+        })
+    })
+
+    // Menghapus data dummy dengan nama update-role
+    afterEach(async () => {
+        await connection.roles.deleteMany({
+            where: {
+                ROLE_ID: { contains: `dummy-role` }
+            }
+        })
+    })
+
+    it('should reject process becouse id role not found', async () => {
+        const result = await supertest(app).put(`/role/rolenotfound`)
+            .send({
+                name: `success-update-role`
+            })
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should rejected becouse failed mandatory validation', async () => {
+        const result = await supertest(app)
+            .put(`/role/update-role`)
+            .send({ test_mandatory: null })
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should rejected becouse status role is inactive', async () => {
+        const result = await supertest(app)
+            .put(`/role/dummy-update`)
+            .send({
+                name: `successed-update-role`
+            })
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should updated data dummy role', async () => {
+        const result = await supertest(app)
+            .put(`/role/dummy-role`)
+            .send({
+                name: `success-update-role`
+            })
+
+        expect(result.status).toBe(200);
+        expect(result.body.message).toBeDefined();
+        expect(result.body.message).toBe("Data role berhasil di update");
+    });
+});
