aea689b112035518ae287800a0d1aa7d1145a864
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-18 06:11:58 +0700

    UPDATE || supados layout sareng judul admin janten responsif

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <title>{{ $title ?? 'Perpustakaan IPDN' }}</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -61,6 +61,7 @@
 
         <section class="section dashboard">
             {{ $slot }}
+            {{--  // TODO: Buatkan module similaritas --}}
         </section>
 
     </main><!-- End #main -->
