f4c37d6fc1055d70a5dc48e69075c9ef64b79836
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-23 08:26:08 +0700

    FIXING BUG || Vendor doesnt running well at admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,7 +39,7 @@
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
 
-    @vite(['./assets/admin/js/main.js', './assets/admin/css/style.css'])
+    @vite(['./resources/css/app.css', './resources/js/app.js'])
 </head>
 
 <body>
@@ -74,15 +74,15 @@
 
 
     <!-- Vendor JS Files -->
-    <script src="./assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    {{--
+    <script src="./assets/admin/vendor/tinymce/tinymce.min.js"></script>
+    <script src="./assets/admin/vendor/quill/quill.min.js"></script>
+    <script src="./assets/admin/vendor/simple-datatables/simple-datatables.js"></script>
     <script src="./assets/admin/vendor/apexcharts/apexcharts.min.js"></script>
     <script src="./assets/admin/vendor/chart.js/chart.umd.js"></script>
     <script src="./assets/admin/vendor/echarts/echarts.min.js"></script>
-    <script src="./assets/admin/vendor/quill/quill.min.js"></script>
-    <script src="./assets/admin/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="./assets/admin/vendor/tinymce/tinymce.min.js"></script>
     <script src="./assets/admin/vendor/php-email-form/validate.js"></script>
+    {{--
+    <script src="./assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     --}}
 
     <!-- Template Main JS File -->
