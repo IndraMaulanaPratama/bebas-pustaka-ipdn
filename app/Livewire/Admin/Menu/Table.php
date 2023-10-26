ec11d25f4c13d862c56366f19b4dbc0bf9434a38
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-26 16:40:35 +0700

    UPDATE || Implement Single Page Apllication on MENU module

--- app/Livewire/Admin/Menu/Table.php
+++ app/Livewire/Admin/Menu/Table.php
@@ -4,14 +4,39 @@ namespace App\Livewire\Admin\Menu;
 
 use App\Models\Menu;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
+use illuminate\Support\Str;
 
 class Table extends Component
 {
     use WithPagination;
 
     public $search = '';
+    public $id = '';
+
+    // Ngadamel sareng marios input data menu ti client
+    #[Rule(['required', 'string', 'max:50', 'unique:MENUS,MENU_NAME'])]
+    public $menu = '';
+
+    // Ngadamel sareng marios input data icon ti client
+    #[Rule(['string', 'max:50'])]
+    public $icon = '';
+
+    // Ngadamel sareng marios input data description ti client
+    #[Rule(['string'])]
+    public $description = '';
+
+    // Ngadamel sareng marios input data url ti client
+    #[Rule(['required', 'string', 'max:20', 'unique:MENUS,MENU_URL'])]
+    public $url = '';
+
+    #[Rule(['string', 'required'])]
+    public $position = '';
+
+
+
 
     /**
      * On () -> Attribut kanggo maca pesan ti componen sanes
@@ -20,33 +45,101 @@ class Table extends Component
      * fungsi placeholder nyaeta fungsi candakan livewire pikeun ngaload ulang component
      */
 
+
+    /**
+     * Fungsi kanggo ngadamel slug url menu
+     */
+    public function updatedMenu()
+    {
+        $this->url = str::slug($this->menu);
+    }
+
+
+    /**
+     * Fungsi kanggo nyimpen data menu enggal
+     */
+
+    public function createData()
+    {
+        // Ngajalankeun validasi data
+        $this->validate();
+
+        try {
+            // Inisialisasi data nu bade di eksekusi
+            $data = [
+                'MENU_ID' => uuid_create(4),
+                'MENU_NAME' => $this->menu,
+                'MENU_DESCRIPTION' => $this->description,
+                'MENU_ICON' => $this->icon != null ? $this->icon : null,
+                'MENU_URL' => str::slug($this->menu),
+                'MENU_POSITION' => $this->position,
+            ];
+
+            // proses nyimpen data ka database
+            Menu::create($data);
+
+            // ngadamel pengumuman kanggo component nu sanes
+            $this->dispatch('menu-created', 'Menu baru berhasil ditambahkan');
+
+            // mulihkeun kondisi form ka posisi default
+            $this->resetForm();
+
+        } catch (\Throwable $th) {
+            $this->dispatch('failed-creating-menu', $th->getMessage());
+        }
+    }
+
+
     #[On('menu-created'), On('menu-updated')]
     public function placeholder()
     {
-        return <<<'HTML'
-        <div>
-            <!-- Loading spinner... -->
-            <svg>...</svg>
-        </div>
-        HTML;
+        return view('components.admin.components.spinner.loading');
     }
 
     public function updateMenu($id)
     {
+        $this->reset();
+
         $menu = Menu::find($id);
 
-        $data = [
-            'id' => $menu->MENU_ID,
-            'menu' => $menu->MENU_NAME,
-            'description' => $menu->MENU_DESCRIPTION,
-            'url' => $menu->MENU_URL,
+        $this->id = $menu->MENU_ID;
+        $this->menu = $menu->MENU_NAME;
+        $this->icon = $menu->MENU_ICON;
+        $this->url = $menu->MENU_URL;
+        $this->description = $menu->MENU_DESCRIPTION;
+        $this->position = $menu->MENU_POSITION;
+        // $this->dispatch('selected-menu', $data);
+    }
 
-            'title' => 'Perbarui Data',
-            'spanTitle' => 'Menu ' . $menu->MENU_NAME,
-            'actionName' => 'updateData',
-        ];
+    /**
+     * Fungsi kanggo ngarobih data anu tos disimpen di database
+     */
+    public function updateData()
+    {
+        $this->validate();
 
-        $this->dispatch('selected-menu', $data);
+        try {
+            // Inisialisasi data nu bade di eksekusi
+            $data = [
+                'MENU_NAME' => $this->menu,
+                'MENU_DESCRIPTION' => $this->description,
+                'MENU_URL' => $this->url,
+                'MENU_ICON' => $this->icon,
+                'MENU_POSITION' => $this->position,
+            ];
+
+            // proses nyimpen data ka database
+            Menu::find($this->id)->update($data);
+
+            // mulihkeun kondisi form ka posisi default
+            $this->resetForm();
+
+            // ngadamel pengumuman kanggo component nu sanes
+            $this->dispatch('menu-updated', 'Menu yang anda pilih, berhasil diperbaharui');
+
+        } catch (\Throwable $th) {
+            $this->dispatch('failed-updating-menu', 'Terjadi kesalahan pada saat memperbaharui data menu');
+        }
     }
 
     public function deleteMenu($id)
@@ -74,13 +167,32 @@ class Table extends Component
     }
 
 
+    /**
+     * Fungsi pikeun mulangkeun kondisi form ka awal
+     */
+
+    public function resetForm()
+    {
+        // Mulangkeun kondisi form sapertos awalna
+        $this->reset();
+
+        // Settingan card form-update menu
+        $this->title = 'Buat Data Baru';
+        $this->spanTitle = ' | Menu';
+        $this->actionName = 'createData';
+    }
+
+
     public function render()
     {
         $menu = Menu::when(
-            $this->search, function ($query, $search) {
-                return $query->where('MENU_NAME','like','%'. $search .'%');
+            $this->search,
+            function ($query, $search) {
+                return $query->where('MENU_NAME', 'like', '%' . $search . '%');
             }
         )->latest()->paginate();
+
+
         return view('livewire.admin.menu.table', ['data' => $menu]);
     }
 }
--- resources/views/livewire/page/menu.blade.php
+++ resources/views/livewire/page/menu.blade.php
@@ -12,18 +12,6 @@
     @endif
 
     <div class="row">
-        <div class="col-8">
-            <livewire:admin.menu.create :title="$title" :spanTitle="$spanTitle" :actionName='$actionName' />
-        </div>
-
-        <div class="col-4">
-            <x-admin.components.card.card size=12>
-                Asign Data Menu
-            </x-admin.components.card.card>
-        </div>
-    </div>
-
-    <div class="row">
         <livewire:admin.menu.table />
     </div>
 
