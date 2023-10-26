babb546b022ccfd3f72f73b28ef7fd080e1c8f88
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-27 03:41:23 +0700

    UPDATE || Refactor component file on users

--- app/Livewire/Admin/Users/Table.php
+++ app/Livewire/Admin/Users/Table.php
@@ -23,15 +23,10 @@ class Table extends Component
     /***
      * Fungsi kanggo ngarefresh table
      */
-    #[On('user-created'), On('user-updated')]
+    #[On('user-created'), On('user-updated'), On('user-deleted')]
     public function placeholder()
     {
-        return <<<'HTML'
-        <div>
-            <!-- Loading spinner... -->
-            <svg>...</svg>
-        </div>
-        HTML;
+        return view("components.admin.components.spinner.loading");
     }
 
 
@@ -58,93 +53,9 @@ class Table extends Component
      */
     public function updateUser($id)
     {
-        $user = User::with('role')->where('id', $id)->first();
-
-        $data = [
-            'id' => $id,
-            'name' => $user->name,
-            'email' => $user->email,
-            'password' => $user->password,
-            'photo' => $user->photo,
-            'sign' => $user->sign,
-        ];
-
-        $this->update_id = $id;
-        $this->update_name = $data['name'];
-        $this->update_email = $data['email'];
-        // dd($data);
+        $this->dispatch('selected-user', $id);
     }
 
-
-
-    /***
-     * Fungsi kanggo ngarobih data user
-     */
-    public function updateData()
-    {
-        try {
-            null != $this->update_photo ? $photoName = Carbon::now()->timestamp . '-' . $this->update_photo->getClientOriginalName() : $photoName = null;
-            null != $this->update_sign ? $signName = Carbon::now()->timestamp . '-' . $this->update_sign->getClientOriginalName() : $signName = null;
-
-
-            $data = [
-                'id' => $this->update_id,
-                'name' => $this->update_name,
-                'email' => $this->update_email,
-                'password' => bcrypt($this->update_password),
-                'photo' => str_replace(" ", "", $photoName),
-                'sign' => str_replace(" ", "", $signName),
-                'user_role' => $this->update_role,
-            ];
-
-            if (null == $this->update_password) {
-                unset($data['password']);
-            }
-            if (null == $this->update_photo) {
-                unset($data['photo']);
-            }
-            if (null == $this->update_sign) {
-                unset($data['sign']);
-            }
-            if (null == $this->update_role) {
-                unset($data['user_role']);
-            }
-
-            // Miwarang livewire kanggo nyimpen data dumasar kana katangtosan nu tos di damel
-            $this->update_photo != null ? $this->update_photo->storeAs('foto_pegawai', str_replace(" ", "", $photoName), 'public') : null;
-            $this->update_sign != null ? $this->update_sign->storeAs('tanda_tangan', str_replace(" ", "", $signName), 'public') : null;
-
-            User::query()->where('id', $this->update_id)->update($data);
-            $this->dispatch('user-updated', 'Data ' . $data['name'] . ' berhasil diperbaharui');
-        } catch (\Throwable $th) {
-            dd('' . $th->getMessage());
-        }
-
-
-
-    }
-
-
-
-    /***
-     * Percobaan kanggo ngosongkeun input file
-     */
-    public function clearFile()
-    {
-        $this->update_photo = rand();
-        $this->update_sign = rand();
-    }
-
-
-
-    /**
-     * Fungsi kanggo ngabersihkeun data formulir
-     */
-    public function resetForm()
-    {
-        $this->clearFile();
-        $this->reset();
-    }
 
 
 
@@ -157,12 +68,6 @@ class Table extends Component
             }
         )->latest()->paginate();
 
-        $role = Role::query()->whereNot('ROLE_NAME', '=', 'Super Admin')->get([
-            'ROLE_ID AS id',
-            'ROLE_NAME AS name'
-        ]);
-
-
-        return view('livewire.admin.users.table', ['data' => $user, 'data_role' => $role]);
+        return view('livewire.admin.users.table', ['data' => $user]);
     }
 }
