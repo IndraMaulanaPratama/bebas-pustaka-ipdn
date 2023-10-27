6d601729ebe8d4588eb3b232483962110c366afb
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-27 15:32:07 +0700

    FIXING BUG || Middleware Access

--- app/Http/Middleware/PembatasanAksesMenu.php
+++ app/Http/Middleware/PembatasanAksesMenu.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Menu;
 use App\Models\pivotMenu;
 use Closure;
 use Illuminate\Http\Request;
@@ -15,6 +16,12 @@ class PembatasanAksesMenu
     {
         $address = Route::currentRouteName(); // <-- Maca halaman anu bade di tuju ku semah
         $role = Auth::user()->role->ROLE_ID; // <-- Maca role anu di candak ku semah
+        $menu = Menu::where("MENU_URL", $address)->first();
+
+        // Validasi menu anu bade di akses
+        if ($menu == null) {
+            return false;
+        }
 
         // Logic kanggo milarian menu anu tiasa diakses ku semah
         $pivot = pivotMenu::with(
@@ -23,8 +30,9 @@ class PembatasanAksesMenu
                     $query->where('MENU_URL', $address);
                 }
             ]
-        )->where('PIVOT_ROLE', $role)->first();
+        )->where(['PIVOT_MENU' => $menu->MENU_ID, 'PIVOT_ROLE' => $role])->first();
 
+        // dd($pivot);
 
         // Upami menu anu di maksad teu kapendak dinu database mangka pulangkeun hasil false
         if (false == count($pivot->menu)) {
