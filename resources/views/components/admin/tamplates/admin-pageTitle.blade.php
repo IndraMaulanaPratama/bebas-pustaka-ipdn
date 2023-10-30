98ccc6a79b273aa5e5742c8c934739ef08ff464e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-30 09:04:59 +0700

    UPDATE || Disabled folder location on breadcrumb

--- resources/views/components/admin/tamplates/admin-pageTitle.blade.php
+++ resources/views/components/admin/tamplates/admin-pageTitle.blade.php
@@ -1,9 +1,9 @@
-<div class="pagetitle">
+<div class="pagetitle mb-4">
     <h1> {{ $title ?? 'Perpustakaan IPDN' }} </h1>
-    <nav>
+    {{-- <nav>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="index.html">Home</a></li>
             <li class="breadcrumb-item active">Dashboard</li>
         </ol>
-    </nav>
+    </nav> --}}
 </div>
