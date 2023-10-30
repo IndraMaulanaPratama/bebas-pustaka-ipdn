15cccd833248f4fc7526512b9f1f79ce655b3bc0
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-30 09:03:30 +0700

    UPDATE || Fiture logic login for praja

--- app/Livewire/Page/Login.php
+++ app/Livewire/Page/Login.php
@@ -2,8 +2,6 @@
 
 namespace App\Livewire\Page;
 
-use App\Models\pivotMenu;
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
@@ -23,21 +21,54 @@ class Login extends Component
     public function login()
     {
         session()->forget('warning');
-        $credentials = $this->validate();
 
-        if (Auth::attempt($credentials)) {
-            session()->regenerate();
-            // Auth::guard('web')->login($credentials);
-            $this->redirect('/');
+        // Maca sumber data login boh ti admin atanapi praja
+        $domain = explode('@', $this->email)[1];
 
-        } else {
-            $this->password = null;
+
+        // Proses upami semah ti pangurus perpustakaan
+        if ($domain === "ipdn.ac.id") {
+            $credentials = $this->validate();
+            if (Auth::attempt($credentials)) {
+                session()->regenerate();
+                // Auth::guard('web')->login($credentials);
+                $this->redirect('/');
+
+            } else {
+                $this->password = null;
+                session()->flash('warning', 'Data pengguna tidak ditemukan');
+            }
+        }
+
+        // Proses upami semah ti praja
+        elseif ($domain === 'praja.ipdn.ac.id') {
+
+            // Milari data praja dumasar kana email sareng password
+            $user = User::where(['email' => $this->email])->first();
+
+            // ngadaptarkeun data login ka session auth
+            if ($user != null) {
+                session()->flash('success', 'Anda berhasil login kedalam aplikasi');
+
+                Auth::login($user);
+                $this->redirect('/');
+                return;
+            }
+
+            // masihkeun pesan error ka semah kumargi data login teu kapendak di system
+            session()->flash('warning', 'Data pengguna tidak ditemukan');
+            return;
+        }
+
+        // Proses Upami domain duka timanten
+        else {
             session()->flash('warning', 'Data pengguna tidak ditemukan');
         }
     }
 
     public function render()
     {
+        session()->reflash();
         return view('livewire.page.login');
     }
 }
