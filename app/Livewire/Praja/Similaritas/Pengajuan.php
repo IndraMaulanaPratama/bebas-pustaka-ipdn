0434d6b0633ca6107f6bb16b925ccf7617156dfa
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-31 18:44:00 +0700

    ADD NEW || Logic for handler page pengajuan, resume and tips

---
+++ app/Livewire/Praja/Similaritas/Pengajuan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Praja\Similaritas;
+
+use Livewire\Component;
+
+class Pengajuan extends Component
+{
+    public function render()
+    {
+        return view('livewire.praja.similaritas.pengajuan');
+    }
+}
---
+++ app/Livewire/Praja/Similaritas/Resume.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Praja\Similaritas;
+
+use Livewire\Component;
+
+class Resume extends Component
+{
+    public function render()
+    {
+        return view('livewire.praja.similaritas.resume');
+    }
+}
