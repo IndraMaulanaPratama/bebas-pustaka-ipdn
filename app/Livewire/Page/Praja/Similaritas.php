ac44e11f94a60c09170513dd3c33e6c1360af1f5
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-31 18:44:26 +0700

    ADD NEW || Logic for handler request similaritas praja page

---
+++ app/Livewire/Page/Praja/Similaritas.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Page\Praja;
+
+use Livewire\Attributes\On;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title("Halaman Similaritas")]
+class Similaritas extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.praja.similaritas');
+    }
+}
