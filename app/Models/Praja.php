cbc2c31c1ad7b20c37c2479d430df42b5cd12768
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-31 09:43:22 +0700

    ADD NEW || Relation to table similaritas on praja model

--- app/Models/Praja.php
+++ app/Models/Praja.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Praja extends Model
 {
     use HasFactory;
+    protected $table = "PRAJA";
+    protected $primaryKey = "PRAJA_NPP";
+    protected $keyType = "string";
+    protected $perPage = 10;
+    protected $fillable = [
+        'PRAJA_NPP',
+        'PRAJA_EMAIL',
+        'PRAJA_NOMOR_PONSEL',
+    ];
+
+
+    //  <--- *** Relation Area *** ---> //
+
+    public function similaritas(): HasOne
+    {
+        return $this->hasOne(Praja::class, 'SIMILARITAS_PRAJA', 'PRAJA_NPP');
+    }
+
+    //  <--- *** End Of Relation Area *** ---> //
+
 }
