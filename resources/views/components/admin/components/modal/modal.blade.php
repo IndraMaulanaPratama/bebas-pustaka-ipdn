2484b7c021122878082db0d099aa72e788cec58f
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-10-31 18:36:51 +0700

    UPDATE || Make Modal components more dynamic with adjusment size

--- resources/views/components/admin/components/modal/modal.blade.php
+++ resources/views/components/admin/components/modal/modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade " id="{{ $id }}" tabindex="-1" aria-labelledby="{{ $id }}" aria-hidden="true"
     wire:ignore>
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-dialog modal-{{ $size ?? null}} modal-dialog-scrollable">
         <div class="modal-content">
             {{ $slot }}
         </div>
