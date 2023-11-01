989b92afcc2c251dee9a964b19baf13d5f92a66e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-01 09:57:35 +0700

    IMPLEMENT || Login using satu data praja

--- app/Livewire/Page/Login.php
+++ app/Livewire/Page/Login.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Page;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
@@ -24,6 +25,7 @@ class Login extends Component
 
         // Maca sumber data login boh ti admin atanapi praja
         $domain = explode('@', $this->email)[1];
+        $npp = explode('@', $this->email)[0];
 
 
         // Proses upami semah ti pangurus perpustakaan
@@ -31,7 +33,6 @@ class Login extends Component
             $credentials = $this->validate();
             if (Auth::attempt($credentials)) {
                 session()->regenerate();
-                // Auth::guard('web')->login($credentials);
                 $this->redirect('/');
 
             } else {
@@ -44,15 +45,49 @@ class Login extends Component
         elseif ($domain === 'praja.ipdn.ac.id') {
 
             // Milari data praja dumasar kana email sareng password
-            $user = User::where(['email' => $this->email])->first();
+            $praja = json_decode(file_get_contents(env('APP_PRAJA') . 'praja?npp=' . $npp), true);
 
-            // ngadaptarkeun data login ka session auth
-            if ($user != null) {
-                session()->flash('success', 'Anda berhasil login kedalam aplikasi');
+            if ($praja) {
+                $credentials = $this->validate();
 
-                Auth::login($user);
-                $this->redirect('/');
-                return;
+                // Milarian data praja ka table user
+                if (Auth::attempt($credentials)) {
+                    session()->regenerate();
+                    $this->redirect('/');
+                }
+
+                // Ngadamel user praja kumargi teu acan ka data di user
+                elseif ($praja['data'][0]['TANGGAL_LAHIR'] == $this->password) {
+
+                    try {
+                        $role = Role::where('ROLE_NAME', 'PRAJA UTAMA')->first();
+
+                        $data = [
+                            'name' => $praja['data'][0]['NAMA'],
+                            'email' => $praja['data'][0]['EMAIL'],
+                            'password' => bcrypt($praja['data'][0]['TANGGAL_LAHIR']),
+                            'photo' => "defaultPraja.png",
+                            'user_role' => $role->ROLE_ID,
+                        ];
+
+                        User::create($data);
+
+                        $credentials = $this->validate();
+                        Auth::attempt($credentials);
+                        session()->regenerate();
+                        $this->redirect('/');
+
+                    } catch (\Throwable $th) {
+                        $this->password = null;
+                        session()->flash('warning', 'Data pengguna tidak ditemukan');
+                    }
+                }
+
+                //  Masihkeun pesan error kumargi data password teu sami sareng data tanggal lahir praja
+                else {
+                    session()->flash('warning', 'Data pengguna tidak ditemukan');
+                    return;
+                }
             }
 
             // masihkeun pesan error ka semah kumargi data login teu kapendak di system
