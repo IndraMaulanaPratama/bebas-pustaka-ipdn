62e93477f31908264dc138caa6e3ca8e6f8e8aee
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-01 16:56:19 +0700

    ADD NEW || Function for showing alert message based on similaritas and update ponsel

--- app/Livewire/Page/Praja/Similaritas.php
+++ app/Livewire/Page/Praja/Similaritas.php
@@ -9,6 +9,22 @@ use Livewire\Component;
 #[Title("Halaman Similaritas")]
 class Similaritas extends Component
 {
+
+    #[On("ponsel-updated"), On("similaritas-created")]
+    public function processSuccessfully($message)
+    {
+        session()->reflash();
+        session()->flash('success', $message);
+    }
+
+
+
+    #[On('failed-updating-ponsel'), On("failed-creating-similaritas")]
+    public function failedProcess($message)
+    {
+        session()->reflash();
+        session()->flash('warning', $message);
+    }
     public function render()
     {
         return view('livewire.page.praja.similaritas');
