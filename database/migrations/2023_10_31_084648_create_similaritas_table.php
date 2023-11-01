cec5e0206f83078d064b2c170ac0c3d437dd5b21
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-01 16:59:25 +0700

    UPDATE || set nullable and remove relation praja on migration similaritas

--- database/migrations/2023_10_31_084648_create_similaritas_table.php
+++ database/migrations/2023_10_31_084648_create_similaritas_table.php
@@ -11,24 +11,23 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('SIMILARITAS', function (Blueprint $table) {
-            $table->string('SIMILARTIAS_ID', 36)->primary();
-            $table->string('SIMILARITAS_NUMBER', 35)->comment('Nomor surat similartias');
-            $table->string('SIMILARITAS_PRAJA', 8)->comment('Foreign Key ke table praja');
-            $table->string('SIMILARITAS_OFFICER', 255)->comment('Foreign Key ke table user sebagai petugas');
+            $table->string('SIMILARITAS_ID', 36)->primary();
+            $table->string('SIMILARITAS_NUMBER', 35)->nullable()->comment('Nomor surat similartias');
+            $table->string('SIMILARITAS_PRAJA', 8)->comment('Foreign Key ke data praja');
+            $table->string('SIMILARITAS_OFFICER', 255)->nullable()->comment('Foreign Key ke table user sebagai petugas');
             $table->text('SIMILARITAS_TITLE');
             $table->string('SIMILARITAS_CLASS', 255)->comment('Kelas di dalam turnitin');
             $table->string('SIMILARITAS_ABSENT', 4)->comment('No Absen didalam turnitin');
-            $table->string('SIMILARITAS_VALUE', 2)->comment('Tingkat persentase similaritas');
-            $table->boolean('SIMILARITAS_BIBLIOGRAFI')->comment('Berisi true dan false');
-            $table->boolean('SIMILARITAS_SMALL_WORD')->comment('Berisi true dan false');
-            $table->string('SIMILARITAS_SMALL_WORD_COUNT')->comment('Ketentuan small word yang diterapkan');
-            $table->boolean('SIMILARITAS_QUOTE')->comment('Berisi true dan false');
-            $table->string('SIMILARITAS_APPROVED')->comment('Tanggal Approve');
+            $table->string('SIMILARITAS_VALUE', 2)->nullable()->comment('Tingkat persentase similaritas');
+            $table->boolean('SIMILARITAS_BIBLIOGRAFI')->nullable()->comment('Berisi true dan false');
+            $table->boolean('SIMILARITAS_SMALL_WORD')->nullable()->comment('Berisi true dan false');
+            $table->string('SIMILARITAS_SMALL_WORD_COUNT')->nullable()->comment('Ketentuan small word yang diterapkan');
+            $table->boolean('SIMILARITAS_QUOTE')->nullable()->comment('Berisi true dan false');
+            $table->string('SIMILARITAS_APPROVED')->nullable()->comment('Tanggal Approve');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('SIMILARITAS_PRAJA')->references('PRAJA_NPP')->on('PRAJA')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('SIMILARITAS_OFFICER')->references('email')->on('users');
+            $table->foreign('SIMILARITAS_OFFICER')->references('id')->on('users');
         });
     }
 
