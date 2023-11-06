8a44e5704ea9b8cfc68ffc690b53f8321a385385
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-07 04:57:37 +0700

    UPDATE || Change max char for field menu url on migration menu

--- database/migrations/2023_10_18_082029_create_menus_table.php
+++ database/migrations/2023_10_18_082029_create_menus_table.php
@@ -12,10 +12,10 @@ return new class extends Migration {
     {
         Schema::create('MENUS', function (Blueprint $table) {
             $table->string('MENU_ID', 36)->default(uuid_create(4))->primary();
-            $table->string('MENU_NAME', 50);
+            $table->string('MENU_NAME', 150);
             $table->string('MENU_ICON', 50)->default('bi-journal-a');
             $table->text('MENU_DESCRIPTION')->nullable(true)->comment('Keterangan menu');
-            $table->string('MENU_URL', 20)->comment('Nama Route Dari Halaman');
+            $table->string('MENU_URL', 150)->comment('Nama Route Dari Halaman');
             $table->string('MENU_POSITION', 30)->default('tautan')->nullable(true)->comment('Keterangan dimana menu ini ini kan ditampilkan');
             $table->timestamps();
             $table->softDeletes();
