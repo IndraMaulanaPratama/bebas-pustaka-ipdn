ebfd57bd7a8662cc386c9cd094d5d3e4b37f79cc
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-07 05:07:45 +0700

    ADD NEW || Rejent module on pinjaman fakultas

---
+++ app/Livewire/Admin/PinjamanFakultas/Reject.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Livewire\Admin\PinjamanFakultas;
+
+use App\Models\PinjamanFakultas;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class Reject extends Component
+{
+
+    public $inputNote;
+    public $pustaka;
+
+
+    #[On("data-selected")]
+    /**
+     * Function kanggo maca data anu di kintun ku halaman tabel
+     */
+    public function getData($data)
+    {
+        $this->pustaka = $data;
+    }
+
+
+    /**
+     * Function kanggo ngarobih data pengajuan
+     */
+    public function rejecting()
+    {
+        try {
+            // Nyandak id pengajuan
+            $id = $this->pustaka['FAKULTAS_ID'];
+
+            // Inisialisasi data anu bade di robih
+            $data = [
+                'FAKULTAS_STATUS' => "Ditolak",
+                'FAKULTAS_NOTES' => $this->inputNote,
+            ];
+
+            // Proses ngarobih data pengajuan
+            PinjamanFakultas::where("FAKULTAS_ID", $id)->update($data);
+
+            // Ngadamel sinyal yen perobihan data pengajuan tos rengse
+            $this->dispatch("data-rejected", "Pengajuan Bebas Pinjaman Fakultas Berhasil Ditolak");
+            $this->reset();
+
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-rejecting-data", $th->getMessage());
+        }
+
+    }
+
+
+
+    /**
+     * Function kanggo mulangkeun kondisi formulir
+     */
+    public function resetForm()
+    {
+        $this->reset();
+    }
+
+
+
+
+    public function render()
+    {
+        return view('livewire.admin.pinjaman-fakultas.reject');
+    }
+}
