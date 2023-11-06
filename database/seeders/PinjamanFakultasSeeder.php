4c8cb546d8ac1cbc9107feeca2caca0f0dbde4ef
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-07 05:00:00 +0700

    ADD NEW || Seeder for table pinjaman fakultas

---
+++ database/seeders/PinjamanFakultasSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PinjamanFakultasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
