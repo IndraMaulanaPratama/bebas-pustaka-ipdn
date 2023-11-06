c514389259f93c745dd183232f0550b4eb2a9870
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-06 15:36:04 +0700

    ADD NEW || Model Pinjaman Pustaka for storing data bebas pinjaman perpustakaan

---
+++ app/Models/PinjamanPustaka.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PinjamanPustaka extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = "PINJAMAN_PUSTAKA";
+    protected $primaryKey = "PUSTAKA_ID";
+    protected $keyType = "string";
+    protected $perPage = 10;
+    protected $with = ['user'];
+
+    protected $fillable = [
+        'PUSTAKA_ID',
+        'PUSTAKA_NUMBER',
+        'PUSTAKA_PRAJA',
+        'PUSTAKA_OFFICER',
+        'PUSTAKA_STATUS',
+        'PUSTAKA_APPROVED',
+        'PUSTAKA_NOTES',
+    ];
+
+
+    // --- *** Ranahna Relasi *** --- //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "SIMILARITAS_OFFICER", "id");
+    }
+
+    // --- *** Tungtung tina Ranahna Relasi *** --- //
+}
