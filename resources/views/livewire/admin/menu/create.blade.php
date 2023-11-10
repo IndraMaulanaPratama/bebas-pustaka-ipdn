da2eb06b7ed2c82d81d738c43a789a0f379fb515
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-10 13:49:13 +0700

    IMPLEMENT || Responsive desain on create menu

--- resources/views/livewire/admin/menu/create.blade.php
+++ resources/views/livewire/admin/menu/create.blade.php
@@ -1,61 +1,65 @@
 {{-- Card kanggo formulir tambih data --}}
-<x-admin.components.card.card size=12 :title="$title" :titleSpan="$spanTitle">
-    <form wire:submit={{ $actionName }} class="form row g-3">
+<x-admin.components.modal.modal id='formCreateMenu'>
+    <x-admin.components.modal.header id='formCreateMenu' title="Form tambah data menu" />
 
-        <input type="hidden" class="form-control" wire:model.live='id' required='required' />
+    <div class="modal-body">
+        <form wire:submit={{ $actionName }} class="form row g-3">
 
-        {{-- Baris kanggo input menu sareng icon --}}
-        <div class="row g-2">
-            <div class="col-6">
-                {{-- Input Menu Name --}}
-                <x-admin.components.form.input name='menu' placeholder='Nama Menu' />
+            <input type="hidden" class="form-control" wire:model.live='id' required='required' />
+
+            {{-- Baris kanggo input menu sareng icon --}}
+            <div class="row g-2">
+                <div class="col-6">
+                    {{-- Input Menu Name --}}
+                    <x-admin.components.form.input name='menu' placeholder='Nama Menu' />
+                </div>
+
+                <div class="col-6">
+                    {{-- Input Icon Menu --}}
+                    <x-admin.components.form.input name='icon' placeholder='Icon Menu' />
+                    <small>
+                        <a href="https://icons.getbootstrap.com/" target="_blank">Lihat referensi icon</a>
+                    </small>
+                </div>
             </div>
 
-            <div class="col-6">
-                {{-- Input Icon Menu --}}
-                <x-admin.components.form.input name='icon' placeholder='Icon Menu' />
-                <small>
-                    <a href="https://icons.getbootstrap.com/" target="_blank">Lihat referensi icon</a>
-                </small>
+            {{-- Baris kanggo input url sareng posisi --}}
+            <div class="row g-2">
+                <div class="col-6">
+                    {{-- Input Url --}}
+                    @php
+                        $host = env('APP_URL') . '/'; // Nyandak alamat aplikasi
+                    @endphp
+                    <x-admin.components.form.input-group name='url' :textGroup="$host" />
+                    <span class="badge border-light border-1 text-black-50">Gambaran url aplikasi</span>
+
+                </div>
+
+                <div class="col-6">
+                    {{-- Input Posisi Menu --}}
+                    <x-admin.components.form.select name='position' required='required' placeholder='Posisi Menu'>
+                        <option value="tautan">Tautan Navigasi</option>
+                        <option value='navbar'>Navbar</option>
+                        <option value="sidebar">Sidebar</option>
+                    </x-admin.components.form.select>
+
+                </div>
             </div>
-        </div>
 
-        {{-- Baris kanggo input url sareng posisi --}}
-        <div class="row g-2">
-            <div class="col-6">
-                {{-- Input Url --}}
-                @php
-                    $host = env('APP_URL') . '/'; // Nyandak alamat aplikasi
-                @endphp
-                <x-admin.components.form.input-group name='url' :textGroup="$host" />
-                <span class="badge border-light border-1 text-black-50">Gambaran url aplikasi</span>
 
-            </div>
+            {{-- Input Description --}}
+            <x-admin.components.form.textarea name='description' placeholder='Deskripsi Menu' tinggi='100' />
+
+
 
             <div class="col-6">
-                {{-- Input Posisi Menu --}}
-                <x-admin.components.form.select name='position' required='required' placeholder='Posisi Menu'>
-                    <option value="tautan">Tautan Navigasi</option>
-                    <option value='navbar'>Navbar</option>
-                    <option value="sidebar">Sidebar</option>
-                </x-admin.components.form.select>
-
+                <x-admin.components.form.button type="submit" color="primary" text="Simpan" />
+                <x-admin.components.form.button-reset functionName='resetForm' />
             </div>
-        </div>
 
+            <div class="col-4">
+            </div>
 
-        {{-- Input Description --}}
-        <x-admin.components.form.textarea name='description' placeholder='Deskripsi Menu' tinggi='100' />
-
-
-
-        <div class="col-6">
-            <x-admin.components.form.button type="submit" color="primary" text="Simpan" />
-            <x-admin.components.form.button-reset functionName='resetForm' />
-        </div>
-
-        <div class="col-4">
-        </div>
-
-    </form>
-</x-admin.components.card.card>
+        </form>
+    </div>
+</x-admin.components.modal.modal>
