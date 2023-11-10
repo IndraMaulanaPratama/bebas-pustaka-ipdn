648d7db5c053345b8dcdcba801745054c9223d6f
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-10 14:10:12 +0700

    UPDATE || More responsive on card component

--- resources/views/components/admin/components/card/card.blade.php
+++ resources/views/components/admin/components/card/card.blade.php
@@ -1,4 +1,4 @@
-<div class="col-{{ $size ?? 4 }}">
+<div class="col-sm-{{ $small ?? 12 }} col-md-{{ $medium ?? 12 }} col-lg-{{ $size ?? 12 }}">
     <div class="card info-card {{ $type ?? 'sales' }}-card"> <!-- sales (biru), revenue (hijau), customers (orange) -->
         <div class="card-body">
 
