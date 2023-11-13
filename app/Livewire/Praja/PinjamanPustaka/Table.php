219c4513d872321942d7192a2fbaade507e49c5b
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-13 10:24:26 +0700

    ADD NEW || Table Component for pinjaman pustaka praja

---
+++ app/Livewire/Praja/PinjamanPustaka/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Praja\PinjamanPustaka;
+
+use Livewire\Component;
+
+class Table extends Component
+{
+    public function render()
+    {
+        return view('livewire.praja.pinjaman-pustaka.table');
+    }
+}
