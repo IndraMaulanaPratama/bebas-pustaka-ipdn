63cf14e5ad22d5c5bc6dda9cac4a4486e62f5d4e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-13 10:21:18 +0700

    UPDATE || URL Get data praja using env variable

--- app/Http/Controllers/SimilaritasController.php
+++ app/Http/Controllers/SimilaritasController.php
@@ -9,7 +9,7 @@ class SimilaritasController extends Controller
 {
     public function getPraja($npp)
     {
-        $data = json_decode(file_get_contents("http://localhost:8001/api/praja?npp=". $npp), true);
+        $data = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $npp), true);
         return response()->json($data['data'][0]['EMAIL']);
     }
 }
