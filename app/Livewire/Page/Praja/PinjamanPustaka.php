da10c66a4950f59e51fca89e9e8235b814810b0a
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-14 14:03:29 +0700

    IMPLEMENT || Signal Success dan Failed on logic page pinjaman pustaka

--- app/Livewire/Page/Praja/PinjamanPustaka.php
+++ app/Livewire/Page/Praja/PinjamanPustaka.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire\Page\Praja;
 
+use Livewire\Attributes\On;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title("Bebas Pinjaman Perpustakaan")]
 class PinjamanPustaka extends Component
 {
+
+    #[On("data-rejected"), On("data-updated"), On("data-created")]
+    public function processSuccessfully($message)
+    {
+        session()->reflash();
+        session()->flash('success', $message);
+    }
+
+
+
+    #[On("failed-rejecting-data"), On("failed-updating-data"),  On("failed-creating-data")]
+    public function failedProcess($message)
+    {
+        session()->reflash();
+        session()->flash('warning', $message);
+    }
+
+
+
     public function render()
     {
         return view('livewire.page.praja.pinjaman-pustaka');
