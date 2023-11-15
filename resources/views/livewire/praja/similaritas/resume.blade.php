cd36997533befd072ed4c1a9df344a50af7d2d5b
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-15 09:16:13 +0700

    FIXING BUG || Position pengajuan ulang on similaritas praja

--- resources/views/livewire/praja/similaritas/resume.blade.php
+++ resources/views/livewire/praja/similaritas/resume.blade.php
@@ -1,82 +1,80 @@
-<div class="table-responsive">
-    <table class="table table-borderless">
-        <tr class="d-flex">
-            <td class="col-sm-4">Nomor Pokok Praja</td>
-            <td>:</td>
-            <td>{{ $praja->NPP }}</td>
-        </tr>
+<table class="table table-borderless">
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Nomor Pokok Praja</td>
+        <td>:</td>
+        <td>{{ $praja->NPP }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Nama Lengkap</td>
-            <td>:</td>
-            <td>{{ $praja->NAMA }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Nama Lengkap</td>
+        <td>:</td>
+        <td>{{ $praja->NAMA }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Tempat, Tanggal Lahir</td>
-            <td>:</td>
-            <td>{{ $praja->TEMPAT_LAHIR }}, {{ $praja->TANGGAL_LAHIR }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Tempat, Tanggal Lahir</td>
+        <td>:</td>
+        <td>{{ $praja->TEMPAT_LAHIR }}, {{ $praja->TANGGAL_LAHIR }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Jenis Kelamin</td>
-            <td>:</td>
-            <td>{{ $praja->JENIS_KELAMIN == 'L' ? 'LAKI-LAKI' : 'PEREMPUAN' }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Jenis Kelamin</td>
+        <td>:</td>
+        <td>{{ $praja->JENIS_KELAMIN == 'L' ? 'LAKI-LAKI' : 'PEREMPUAN' }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Agama</td>
-            <td>:</td>
-            <td>{{ $praja->AGAMA }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Agama</td>
+        <td>:</td>
+        <td>{{ $praja->AGAMA }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td colspan="3">&nbsp;</td>
-        </tr>
+    <tr>
+        <td colspan="3">&nbsp;</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Judul Skripsi</td>
-            <td>:</td>
-            <td>{{ $similaritas->SIMILARITAS_TITLE ?? '-' }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Judul Skripsi</td>
+        <td>:</td>
+        <td>{{ $similaritas->SIMILARITAS_TITLE ?? '-' }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Kelas <i>Turnitin</i></td>
-            <td>:</td>
-            <td>{{ $similaritas->SIMILARITAS_CLASS ?? '-' }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Kelas <i>Turnitin</i></td>
+        <td>:</td>
+        <td>{{ $similaritas->SIMILARITAS_CLASS ?? '-' }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Nomor Absen <i>Turnitin</i></td>
-            <td>:</td>
-            <td>{{ $similaritas->SIMILARITAS_ABSENT ?? '-' }}</td>
-        </tr>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Nomor Absen <i>Turnitin</i></td>
+        <td>:</td>
+        <td>{{ $similaritas->SIMILARITAS_ABSENT ?? '-' }}</td>
+    </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Status Pengajuan</td>
-            <td>:</td>
-            <td>
-                <div class="position-relative">
-                    <div class="position-absolute start-0">
-                        {{ $similaritas->SIMILARITAS_STATUS ?? '-' }}
-                    </div>
-
-                    <div class="position-absolute end-0">
-                        <button type="button" class="btn btn-rounded-pill btn-outline-secondary btn-sm {{ $buttonAjukan }}"
-                            wire:click='pengajuanUlang("{{ $similaritas->SIMILARITAS_ID ?? null }}")'
-                            wire:confirm='Ajukan kembali perubahan yang sudah dilakukan di aplikasi turnitin?'>
-                            <i class="bi bi-arrow-clockwise"></i>Ajukan Ulang
-                        </button>
-
-                    </div>
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Status Pengajuan</td>
+        <td>:</td>
+        <td>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    {{ $similaritas->SIMILARITAS_STATUS ?? '-' }}
                 </div>
-            </td>
-        </tr>
 
-        <tr class="d-flex">
-            <td class="col-sm-4">Catatan Pengajuan</td>
-            <td>:</td>
-            <td>{{ $similaritas->SIMILARITAS_NOTES ?? '-' }}</td>
-        </tr>
-    </table>
-</div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <button type="button"
+                        class="btn btn-rounded-pill btn-outline-secondary btn-sm {{ $buttonAjukan }}"
+                        wire:click='pengajuanUlang("{{ $similaritas->SIMILARITAS_ID ?? null }}")'
+                        wire:confirm='Ajukan kembali perubahan yang sudah dilakukan di aplikasi turnitin?'>
+                        <i class="bi bi-arrow-clockwise"></i>Ajukan Ulang
+                    </button>
+                </div>
+            </div>
+        </td>
+    </tr>
+
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Catatan Pengajuan</td>
+        <td>:</td>
+        <td>{{ $similaritas->SIMILARITAS_NOTES ?? '-' }}</td>
+    </tr>
+</table>
