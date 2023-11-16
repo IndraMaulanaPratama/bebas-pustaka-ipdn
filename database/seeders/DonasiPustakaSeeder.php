1c1b700119f40df277cc095a31d054729de12d0c
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-16 10:15:49 +0700

    ADD NEW || Seeder donasi perpustakaan

---
+++ database/seeders/DonasiPustakaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DonasiPustakaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
