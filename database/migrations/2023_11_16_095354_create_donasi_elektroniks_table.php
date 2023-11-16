91d25c0c9a13573ba75f8d4ab2d96895e30deb32
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-16 10:23:28 +0700

    ADD NEW || Migration donasi elektronik

---
+++ database/migrations/2023_11_16_095354_create_donasi_elektroniks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DONASI_ELEKTRONIK', function (Blueprint $table) {
+            $table->string('ELEKTRONIK_ID', 36)->primary()->comment('Primary Key untuk table donasi buku elektronik');
+            $table->string('ELEKTRONIK_PRAJA', 8)->comment('Foreign Key ke data praja');
+            $table->unsignedBigInteger('ELEKTRONIK_OFFICER');
+            $table->enum('ELEKTRONIK_STATUS', ['Proses', 'Disetujui', 'Ditolak'])->default('Proses');
+            $table->string('ELEKTRONIK_APPROVED')->nullable()->comment('Tanggal Approve');
+            $table->text('ELEKTRONIK_NOTES')->nullable()->comment('Digunakan untuk memberikan keterangan saat pengajuan di tolak');
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->foreign('ELEKTRONIK_OFFICER')->references('id')->on('users');
+            $table->unsignedBigInteger('ELEKTRONIK_OFFICER', 255)->nullable()->change()->comment('Foreign Key ke table user sebagai petugas');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DONASI_ELEKTRONIK');
+    }
+};
