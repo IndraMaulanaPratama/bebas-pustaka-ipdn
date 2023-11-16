c4e320dcc2de6443e0c3399b380ce2ed844f23fd
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-16 15:22:01 +0700

    ADD NEW || Form rejecting submission donasi buku pustaka

---
+++ resources/views/livewire/admin/donasi-pustaka/reject.blade.php
@@ -0,0 +1,27 @@
+{{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<x-admin.components.modal.modal id='formReject'>
+    <x-admin.components.modal.header id='formReject' title="Tolak pengajuan donasi buku perpustakaan" />
+
+    <form wire:submit='rejecting' method="POST">
+
+        <div class="row g-2 p-2">
+            &nbsp;
+
+            <x-admin.components.form.textarea name='inputNote' placeholder='Tuliskan Alasan Penolakan' />
+
+            {{-- Tombol Reset sareng Submit --}}
+            <div class="modal-footer">
+                {{-- Tombol Reset / Cancel --}}
+                <button type="button" wire:click='resetForm' class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    Batalkan
+                </button>
+
+                {{-- Tombol Simpan / Submit --}}
+                <button type="submit" class="btn btn-outline-primary" data-bs-dismiss="modal">
+                    Simpan
+                </button>
+            </div>
+        </div>
+    </form>
+
+</x-admin.components.modal.modal>
