ff0138142df704fe5196140ff17774de2ed008fb
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-17 10:56:03 +0700

    ADD NEW || Logic for data table donasi fakultas

---
+++ app/Livewire/Admin/DonasiFakultas/Table.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Livewire\Admin\DonasiFakultas;
+
+use App\Models\Akses;
+use App\Models\DonasiFakultas;
+use App\Models\Menu;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class Table extends Component
+{
+
+    public $accessReject, $accessApprove, $accessExport, $accessPrint;
+    public $sortStatus, $sortFakultas, $angkatan, $search;
+    public $npp,
+    $dataPraja,
+    $prajaNama,
+    $prajaEmail,
+    $prajaTempatTanggalLahir,
+    $prajaJenisKelamin,
+    $prajaProvinsi,
+    $prajaKota,
+    $prajaTingkat,
+    $prajaAngkatan,
+    $prajaKampus,
+    $prajaWisma,
+    $prajaPropen,
+    $prajaFakultas,
+    $prajaProdi,
+    $prajaKelas,
+    $prajaPonsel;
+
+
+
+    public function mount()
+    {
+        $roleLogin = Auth::user()->user_role;
+        $url = Route::getCurrentRoute()->action['as']; // Maca nami route anu nuju di buka
+        $menu = Menu::where("MENU_URL", $url)->first();
+
+        $access = Akses::
+            join("PIVOT_MENU", "ACCESSES.ACCESS_MENU", '=', "PIVOT_MENU.PIVOT_ID")
+            ->where(['PIVOT_MENU.PIVOT_MENU' => $menu->MENU_ID, 'PIVOT_MENU.PIVOT_ROLE' => $roleLogin])
+            ->first();
+
+        $this->accessApprove = $this->generateAccess($access->ACCESS_APPROVE);
+        $this->accessReject = $this->generateAccess($access->ACCESS_REJECT);
+        $this->accessPrint = $this->generateAccess($access->ACCESS_PRINT);
+        $this->accessExport = $this->generateAccess($access->ACCESS_EXPORT);
+    }
+
+
+
+    public function generateAccess($value)
+    {
+        return $value == 1 ? null : 'invisible';
+    }
+
+
+
+    public function detailPraja($npp)
+    {
+        $detailPraja = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $npp), true);
+        $this->dataPraja = $detailPraja["data"][0];
+
+        $tanggalLahir = Carbon::createFromFormat("Y-m-d", $this->dataPraja["TANGGAL_LAHIR"])->format("d M Y");
+        $jenisKelamin = $this->dataPraja['JENIS_KELAMIN'] == "P" ? "PEREMPUAN" : "LAKI-LAKI";
+
+        $userPraja = User::where('email', $npp . '@praja.ipdn.ac.id')->first();
+        $nomorPonsel = $userPraja->nomor_ponsel;
+
+        $this->prajaNama = $this->dataPraja['NAMA'];
+        $this->prajaEmail = $this->dataPraja['EMAIL'];
+        $this->prajaPonsel = $nomorPonsel;
+        $this->prajaTempatTanggalLahir = $this->dataPraja['TEMPAT_LAHIR'] . ', ' . $tanggalLahir;
+        $this->prajaJenisKelamin = $jenisKelamin;
+        $this->prajaProvinsi = $this->dataPraja['PROVINSI'];
+        $this->prajaKota = $this->dataPraja['KOTA'];
+        $this->prajaTingkat = $this->dataPraja['TINGKAT'];
+        $this->prajaAngkatan = $this->dataPraja['ANGKATAN'];
+        $this->prajaKampus = $this->dataPraja['KAMPUS'];
+        $this->prajaWisma = $this->dataPraja['WISMA'];
+
+        $this->prajaPropen = $this->dataPraja['PROGRAM_PENDIDIKAN'];
+        $this->prajaFakultas = $this->dataPraja['FAKULTAS'];
+        $this->prajaProdi = $this->dataPraja['PROGRAM_STUDI'];
+        $this->prajaKelas = $this->dataPraja['KELAS'];
+    }
+
+
+
+    public function approveData($id)
+    {
+        try {
+            $data = [
+                'FAKULTAS_STATUS' => "Disetujui",
+                'FAKULTAS_NOTES' => null,
+                'FAKULTAS_APPROVED' => Carbon::now("Asia/Jakarta")->format("Y-m-d H:i:s"),
+            ];
+            DonasiFakultas::where("FAKULTAS_ID", $id)->update($data);
+
+            $this->dispatch("data-updated", "Pengajuan donasi buku cetak perpustakaan fakultas berhasil disetujui");
+            $this->reset();
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-updating-data", $th->getMessage());
+        }
+    }
+
+
+
+    public function rejectData($id)
+    {
+        $data = DonasiFakultas::where('FAKULTAS_ID', $id)->first();
+        $this->dispatch('data-selected', $data);
+    }
+
+
+
+    #[On("data-rejected"), On("failed-updating-data"), On("data-updated")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+    public function render()
+    {
+        $fakultas = DonasiFakultas::
+            when(
+                // <!-- Pilari data pengajuan dumasar kana status
+                $this->sortStatus,
+                function ($query, $status) {
+                    return $query->where("FAKULTAS_STATUS", $status);
+                }
+            )
+            ->when(
+                // <!-- Pilari data pengajuan dumasar kana fakultas
+                $this->sortFakultas,
+                function ($query, $fakultas) {
+                    return $query->where("FAKULTAS_NUMBER", "LIKE", '%' . $fakultas . '%');
+                }
+            )
+            ->when(
+                // <!-- Pilari data pengajuan dumasar kana npp
+                $this->search,
+                function ($query, $npp) {
+                    return $query->where("FAKULTAS_PRAJA", "LIKE", $npp . "%");
+                }
+            )
+            ->when(
+                // <!-- Pilari data pengajuan dumasar kana npp
+                $this->angkatan,
+                function ($query, $angkatan) {
+                    return $query->where("FAKULTAS_PRAJA", "LIKE", $angkatan . "%");
+                }
+            )
+            ->latest()
+            ->paginate();
+        return view('livewire.admin.donasi-fakultas.table', [
+            'fakultas' => $fakultas
+        ]);
+    }
+}
