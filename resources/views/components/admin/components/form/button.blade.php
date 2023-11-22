09eb77f48c54100d68b4855cd3670499e661d833
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:53:23 +0700

    UPDATE || add variable size on component button

--- resources/views/components/admin/components/form/button.blade.php
+++ resources/views/components/admin/components/form/button.blade.php
@@ -1,3 +1,3 @@
-<button type="{{ $type ?? 'button' }}" class="btn btn-outline-{{ $color ?? 'secondary' }} {{ $invisible ?? null}} ">
+<button type="{{ $type ?? 'button' }}" class="btn btn-outline-{{ $color ?? 'secondary' }} {{ $invisible ?? null}} btn-{{$size ?? 'md'}} ">
     <small>{{ $text ?? 'button' }}</small>
 </button>
