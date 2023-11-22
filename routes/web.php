a3d40c0dac1f806cb6fba3a1538db26a138013ff
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:55:47 +0700

    UPDATE || Registering donasi praja, admin donasi elektronik, admin survey, admin konten literasi

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,17 @@
 
 use App\Livewire\Page\Admin\Assign;
 use App\Livewire\Page\Admin\Blank;
+use App\Livewire\Page\Admin\DemoDashboard;
+use App\Livewire\Page\Admin\DonasiElektronik;
 use App\Livewire\Page\Admin\DonasiFakultas;
 use App\Livewire\Page\Admin\DonasiPustaka;
+use App\Livewire\Page\Admin\KontenLiterasi;
 use App\Livewire\Page\Admin\PinjamanFakultas;
 use App\Livewire\Page\Admin\PinjamanPustaka;
 use App\Livewire\Page\Admin\Role;
 use App\Livewire\Page\Admin\Similaritas;
 // use App\Livewire\Page\Praja\Similaritas as PrajaSimilaritas;
+use App\Livewire\Page\Admin\Survey;
 use App\Livewire\Page\Admin\Users;
 use App\Livewire\Page\Dashboard;
 use App\Livewire\Page\Login;
@@ -25,12 +29,9 @@ Route::get('/', Dashboard::class)->middleware('auth')->name('/');
 Route::middleware(['auth', 'access'])->group(function () {
     // --- *** Praja Area *** --- //
     Route::get('/praja/similaritas', App\Livewire\Page\Praja\Similaritas::class)->name('praja-similaritas');
-    Route::get('/praja/bebas-pinjaman', \App\Livewire\Page\Praja\PinjamanPustaka::class)->name('praja-pinjaman.perpustakaan');
+    Route::get('/praja/bebas-pinjaman', \App\Livewire\Page\Praja\Pinjaman::class)->name('praja-pinjaman');
+    Route::get('/praja/donasi', \App\Livewire\Page\Praja\Donasi::class)->name('praja-donasi');
     //
-    Route::get('praja/bebas-pinjaman-fakultas', \App\Livewire\Page\Praja\Blank::class)->name('praja-pinjaman.fakultas');
-    Route::get('praja/donasi-buku-perpustakaan', \App\Livewire\Page\Praja\Blank::class)->name('praja-donasi.perpustakaan');
-    Route::get('praja/donasi-buku-fakultas', \App\Livewire\Page\Praja\Blank::class)->name('praja-donasi.fakultas');
-    Route::get('praja/donasi-buku-elektronik', \App\Livewire\Page\Praja\Blank::class)->name('praja-donasi.elektronik');
     Route::get('praja/survey', \App\Livewire\Page\Praja\Blank::class)->name('praja-survey');
     Route::get('praja/konten-literasi', \App\Livewire\Page\Praja\Blank::class)->name('praja-konten.literasi');
     Route::get('praja/unggah-repository', \App\Livewire\Page\Praja\Blank::class)->name('praja-unggah.repository');
@@ -39,15 +40,16 @@ Route::middleware(['auth', 'access'])->group(function () {
 
 
     // --- *** Officer Area *** --- //
+    Route::get('/dashboard', DemoDashboard::class)->name('admin-dashboard');
     Route::get('/similaritas', Similaritas::class)->name('admin-similaritas'); // TODO: Fitur Print dan export
     Route::get('/bebas-pinjaman-perpustakaan', PinjamanPustaka::class)->name('admin-pinjaman.perpustakaan');
     Route::get('/bebas-pinjaman-fakultas', PinjamanFakultas::class)->name('admin-pinjaman.fakultas'); // TODO:: Fitur Print dan export
     Route::get('/donasi-buku-perpustakaan', DonasiPustaka::class)->name('admin-donasi.perpustakaan');
     Route::get('/donasi-buku-fakultas', DonasiFakultas::class)->name('admin-donasi.fakultas');
+    Route::get('/donasi-buku-elektronik', DonasiElektronik::class)->name('admin-donasi.elektronik');
+    Route::get('/survey', Survey::class)->name('admin-survey');
+    Route::get('/konten-literasi', KontenLiterasi::class)->name('admin-konten.literasi'); // TODO:: Buatkan database setting apps
     //
-    Route::get('/donasi-buku-elektronik', Blank::class)->name('admin-donasi.elektronik');
-    Route::get('/survey', Blank::class)->name('admin-survey');
-    Route::get('/konten-literasi', Blank::class)->name('admin-konten.literasi');
     Route::get('/unggah-repository', Blank::class)->name('admin-unggah.repository');
     Route::get('/hard-copy-skripsi', Blank::class)->name('admin-hard.skripsi');
     Route::get('/soft-copy-skripsi', Blank::class)->name('admin-soft.skripsi');
