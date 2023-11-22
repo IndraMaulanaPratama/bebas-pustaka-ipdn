de2ff958aa6553ac0d1e667bf98d713616868268
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:36:13 +0700

    ADD NEW || Model for konten literasi

---
+++ app/Models/KontenLiterasi.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class KontenLiterasi extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = "KONTEN_LITERASI";
+    protected $primaryKey = "KONTEN_ID";
+    protected $keyType = "string";
+    protected $perPage = 10;
+    protected $with = ['user'];
+    protected $fillable = [
+        'KONTEN_ID',
+        'KONTEN_URL',
+        'KONTEN_PRAJA',
+        'KONTEN_OFFICER',
+        'KONTEN_STATUS',
+        'KONTEN_APPROVED',
+        'KONTEN_NOTES',
+    ];
+
+
+    // --- *** Ranahna Relasi *** --- //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "KONTEN_OFFICER", "id");
+    }
+    // --- *** END OF Ranahna Relasi *** --- //
+}
