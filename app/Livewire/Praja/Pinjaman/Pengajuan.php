bd2ae560578ba617f1d5f16a6d2372ceaa0a7e2d
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:50:21 +0700

    ADD NEW ||  Pengajuan Pinjaman for Praja

---
+++ app/Livewire/Praja/Pinjaman/Pengajuan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Praja\Pinjaman;
+
+use Livewire\Component;
+
+class Pengajuan extends Component
+{
+    public function render()
+    {
+        return view('livewire.praja.pinjaman.pengajuan');
+    }
+}
---
+++ resources/views/livewire/praja/pinjaman/pengajuan.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <ol type="a">
+        <li><b>Lokasi Verifikasi</b></li>
+        <p>Perpustakaan Pusat IPDN (Ruang Koleksi Sivitas Akademika Lt. 2) dan Perpustakaan Fakultas</p>
+
+        <li><b>Petunjuk</b></li>
+        <p>Silahkan Praja menghadap petugas perpustakaan kemudian sebutkan Nama beserta NPP untuk dilakukan pengecekan
+            pada sistem otomasi perpustakaan. Praja akan mendapatkan paraf dan cap verifikasi petugas jika tidak
+            mempunyai denda keterlambatan pengembalian buku, namun jika Praja mempunyai tanggungan berupa denda
+            keterlambatan pengembalian buku silahkan dilakukan penyelesaian dengan membayar denda sesuai dengan nominal
+            yang tercatat pada sistem otomasi perpustakaan disertasi dengan mengembalikan koleksi yang dipinjamnya.
+            Adapun ketentuan koleksi yang tidak dapat dikembalikan dikarenakan satu dan lain hal, silahkan Praja
+            melakukan penggantian koleksi tersebut dengan koleksi yang sama atau koleksi dengan subjek yang sejenis.</p>
+
+        <li><b>Pengajuan</b></li>
+        <p>silahkan klik tombol <button class="btn btn-outline-primary btn-sm"> Buat Pengajuan </button> untuk melakukan pengajuan pemeriksaan bebas pinjaman perpustakaan </p>
+
+    </ol>
+</div>
