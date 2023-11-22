2549b6d7e058b2ca9dbf0017acc22ee6a23ecd20
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:49:49 +0700

    ADD NEW ||  Pengajuan Donasi for Praja

---
+++ app/Livewire/Praja/Donasi/Pengajuan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Praja\Donasi;
+
+use Livewire\Component;
+
+class Pengajuan extends Component
+{
+    public function render()
+    {
+        return view('livewire.praja.donasi.pengajuan');
+    }
+}
---
+++ resources/views/livewire/praja/donasi/pengajuan.blade.php
@@ -0,0 +1,50 @@
+<div>
+    <ol type="1">
+        <li>
+            <b>Donasi Cetak Perpustakaan Fakultas</b>
+
+            <ol type="a">
+                <li><b>Lokasi Verifikasi</b></li>
+                <p>Perpustakan Fakultas</p>
+
+
+                <li><b>Petunjuk</b></li>
+                <p>
+                    Silahkan lakukan konfirmasi kepada Bapak/Ibu Pustakawan Perpustakaan Fakultas masing-masing,
+                    kemudian silahkan Praja melakukan donasi sesuai dengan kebijakan Perpustakaan Fakultas tersebut.
+                </p>
+            </ol>
+        </li>
+
+        <li>
+            <b>Donasi Cetak Perpustakaan Pusat</b>
+
+            <ol type="a">
+                <li><b>Lokasi Verifikasi</b></li>
+                <p>Perpustakaan Pusat Ruang Koleksi Sivitas Akademika Lt. 2</p>
+
+                <li><b>Petunjuk</b></li>
+                <p>Menyusul</p>
+            </ol>
+        </li>
+
+        <li>
+            <b>Donasi Poin Perpustakaan Pusat</b>
+
+            <ol type="a">
+                <li><b>Lokasi Verifikasi</b></li>
+                <p>Perpustakaan Pusat</p>
+
+                <li><b>Petunjuk</b></li>
+                <p>
+                    Donasi elektronik dilakukan melalui aplikasi <b>Digilib Praja</b> yang dapat Praja unduh dan <i>install</i> melalui <i>handphone</i> dan atau laptop masing-masing. Selanjutnya seluruh Praja silahkan melakukan pendaftaran akun Digilib Praja dan tunggu hingga dilakukan aktivasi akun oleh Admin.
+                    Kemudian silahkan <b>masuk (login)</b> dan lakukan <b>donasi poin sebesar Rp. 50.000,-</b> pada menu <b>Donasi</b>. Kemudian Praja klik pada tombol <b>checkout</b> dan selanjutnya lakukan pembayaran sesuai dengan jenis pembayaran yang dipilih. Jika status pembayaran sudah berhasil maka donasi poin Praja akan masuk pada sistem Admin Digilib Praja dan Praja dapat melakukan verifikasi kepada Petugas Perpustakaan Pusat IPDN Jatinangor.
+                </p>
+            </ol>
+        </li>
+
+        <li><b>Pengajuan</b></li>
+        <p>silahkan klik tombol <button class="btn btn-outline-primary btn-sm"> Buat Pengajuan </button> untuk melakukan pengajuan pemeriksaan donasi perpustakaan </p>
+
+    </ol>
+</div>
