359c8ddcdf2e89067dd71168946f216571553354
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:45:55 +0700

    ADD NEW || Main Page Survey for admin page

---
+++ app/Livewire/Page/Admin/Survey.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire\Page\Admin;
+
+use Livewire\Attributes\On;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title("Pengaturan Formulir Survey Praja")]
+class Survey extends Component
+{
+
+    #[On("data-rejected"), On("data-updated")]
+    public function processSuccessfully($message)
+    {
+        session()->reflash();
+        session()->flash('success', $message);
+    }
+
+
+
+    #[On("failed-rejecting-data"), On("failed-updating-data")]
+    public function failedProcess($message)
+    {
+        session()->reflash();
+        session()->flash('warning', $message);
+    }
+
+
+
+    public function render()
+    {
+        return view('livewire.page.admin.survey');
+    }
+}
