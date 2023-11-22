41fdfce7662e2de9ee64b1b443f81398b7c36ad7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-23 00:41:09 +0700

    ADD NEW || Reject Konten Literasi

---
+++ app/Livewire/Admin/KontenLiterasi/Reject.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Livewire\Admin\KontenLiterasi;
+
+use App\Models\KontenLiterasi;
+use Illuminate\Support\Carbon;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class Reject extends Component
+{
+    public $inputNote, $konten;
+
+    #[On("data-selected")]
+    /**
+     * Function kanggo maca data anu di kintun ku halaman tabel
+     */
+    public function getData($data)
+    {
+        $this->konten = $data;
+    }
+
+
+
+    /**
+     * Function kanggo ngarobih data pengajuan
+     */
+    public function rejecting()
+    {
+        try {
+            // Nyandak id pengajuan
+            $id = $this->konten['KONTEN_ID'];
+
+            // Inisialisasi data anu bade di robih
+            $data = [
+                'KONTEN_STATUS' => "Ditolak",
+                'KONTEN_NOTES' => $this->inputNote,
+                'KONTEN_APPROVED' => Carbon::now('Asia/Jakarta')->format("Y-m-d H:i:s"),
+            ];
+
+            // Proses ngarobih data pengajuan
+            KontenLiterasi::where("KONTEN_ID", $id)->update($data);
+
+            // Ngadamel sinyal yen perobihan data pengajuan tos rengse
+            $this->dispatch("data-rejected", "Pengajuan konten literasi berhasil ditolak");
+            $this->reset();
+
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-rejecting-data", $th->getMessage());
+        }
+
+    }
+
+
+
+    /**
+     * Function kanggo mulangkeun kondisi formulir
+     */
+    public function resetForm()
+    {
+        $this->reset();
+    }
+
+
+
+    public function render()
+    {
+        return view('livewire.admin.konten-literasi.reject');
+    }
+}
