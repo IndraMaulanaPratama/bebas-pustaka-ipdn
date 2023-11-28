951aa7691d2b7ec5552d8fa78a182f75df5c9a82
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:59:02 +0700

    ADD NEW || Logic for Process Donasi Praja

--- app/Livewire/Praja/Donasi/Pengajuan.php
+++ app/Livewire/Praja/Donasi/Pengajuan.php
@@ -2,10 +2,94 @@
 
 namespace App\Livewire\Praja\Donasi;
 
+use App\Models\DonasiElektronik;
+use App\Models\DonasiFakultas;
+use App\Models\DonasiPustaka;
+use App\Models\PivotDonasi;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Pengajuan extends Component
 {
+    public $praja, $npp, $donasi;
+    public $buttonCreate;
+
+
+
+    #[On("data-created"), On("failed-creating-data")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+
+
+    public function buatPengajuan()
+    {
+        try {
+
+            $data_pustaka = [
+                'PUSTAKA_ID' => uuid_create(4),
+                'PUSTAKA_PRAJA' => $this->npp,
+                'PUSTAKA_OFFICER' => 1,
+                'PUSTAKA_STATUS' => 'Proses'
+            ];
+
+            DonasiPustaka::create($data_pustaka);
+
+            $data_fakultas = [
+                'FAKULTAS_ID' => uuid_create(4),
+                'FAKULTAS_NUMBER' => 'number',
+                'FAKULTAS_PRAJA' => $this->npp,
+                'FAKULTAS_OFFICER' => 1,
+                'FAKULTAS_STATUS' => 'Proses'
+            ];
+
+            DonasiFakultas::create($data_fakultas);
+
+            $data_elektronik = [
+                'ELEKTRONIK_ID' => uuid_create(4),
+                'ELEKTRONIK_NUMBER' => 'number',
+                'ELEKTRONIK_PRAJA' => $this->npp,
+                'ELEKTRONIK_OFFICER' => 1,
+                'ELEKTRONIK_STATUS' => 'Proses'
+            ];
+
+            DonasiElektronik::create($data_elektronik);
+
+
+            $data_pivot = [
+                'PIVOT_ID' => uuid_create(4),
+                'PIVOT_PRAJA' => $this->npp,
+                'PIVOT_PUSTAKA' => $data_pustaka['PUSTAKA_ID'],
+                'PIVOT_FAKULTAS' => $data_fakultas['FAKULTAS_ID'],
+                'PIVOT_ELEKTRONIK' => $data_elektronik['ELEKTRONIK_ID'],
+            ];
+
+            PivotDonasi::create($data_pivot);
+
+            $this->buttonCreate = 'disabled';
+
+            $this->dispatch("data-created", "Pengajuan donasi perpustakaan anda berhasil disimpan");
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-creating-data", $th->getMessage());
+        }
+    }
+
+
+
+    public function mount()
+    {
+        $this->npp = explode("@", Auth::user()->email)[0];
+        $this->praja = User::where("id", Auth::user()->id)->first();
+        $this->donasi = PivotDonasi::where('PIVOT_PRAJA', $this->npp)->first();
+        $this->buttonCreate = $this->donasi == null ? true : 'disabled';
+    }
+
+
+
     public function render()
     {
         return view('livewire.praja.donasi.pengajuan');
--- app/Livewire/Praja/Donasi/Resume.php
+++ app/Livewire/Praja/Donasi/Resume.php
@@ -2,14 +2,50 @@
 
 namespace App\Livewire\Praja\Donasi;
 
+use App\Models\DonasiElektronik;
+use App\Models\DonasiFakultas;
+use App\Models\DonasiPustaka;
 use App\Models\PivotDonasi;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Resume extends Component
 {
     public $praja, $npp, $dataDonasi;
-    public $buttonPengajuan;
+
+    public $buttonResendPustaka = 'hidden',
+    $buttonResendFakultas = 'hidden',
+    $buttonResendElektronik = 'hidden';
+
+
+
+    #[On("failed-updating-data"), On("data-updated"), On("data-created"), On("failed-creating-data")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+
+
+    public function resendPengajuan($table)
+    {
+        try {
+            if ('fakultas' == $table) {
+                DonasiFakultas::where('FAKULTAS_PRAJA', $this->npp)->update(['FAKULTAS_STATUS' => 'Proses']);
+            } elseif ('pustaka' == $table) {
+                DonasiPustaka::where('PUSTAKA_PRAJA', $this->npp)->update(['PUSTAKA_STATUS' => 'Proses']);
+            } elseif ('elektronik' == $table) {
+                DonasiElektronik::where('ELEKTRONIK_PRAJA', $this->npp)->update(['ELEKTRONIK_STATUS' => 'Proses']);
+            }
+
+            $this->dispatch("data-updated", "Pengajuan donasi perpustakaan anda berhasil diajukan ulang");
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-creating-data", $th->getMessage());
+        }
+    }
+
+
 
     public function mount()
     {
@@ -19,14 +55,22 @@ class Resume extends Component
         // Nyandak data praja ka server satu praja dumasar kana npp
         $praja = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $this->npp));
         $this->praja = $praja->data[0];
-
-        $this->dataDonasi = PivotDonasi::where('PIVOT_PRAJA', $this->npp)->first();
-        $this->dataDonasi == null ? $this->buttonPengajuan = null : $this->buttonPengajuan = 'hidden';
     }
+
+
 
     public function render()
     {
+        $this->dataDonasi = PivotDonasi::where('PIVOT_PRAJA', $this->npp)->first();
+
+        if ($this->dataDonasi != null) {
+            $this->buttonResendPustaka = $this->dataDonasi->donasi_pustaka->PUSTAKA_STATUS != 'Ditolak' ? 'hidden' : null;
+            $this->buttonResendFakultas = $this->dataDonasi->donasi_fakultas->FAKULTAS_STATUS != 'Ditolak' ? 'hidden' : null;
+            $this->buttonResendElektronik = $this->dataDonasi->donasi_elektronik->ELEKTRONIK_STATUS != 'Ditolak' ? 'hidden' : null;
+        }
+
         return view('livewire.praja.donasi.resume', [
+            'data' => $this->dataDonasi,
             'praja' => $this->praja
         ]);
     }
