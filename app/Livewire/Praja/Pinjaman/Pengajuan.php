ea60f0998b1f7d2eb37eb0df6f51f8f7142974e7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:59:21 +0700

    ADD NEW || Logic for pinjaman praja

--- app/Livewire/Praja/Pinjaman/Pengajuan.php
+++ app/Livewire/Praja/Pinjaman/Pengajuan.php
@@ -2,10 +2,81 @@
 
 namespace App\Livewire\Praja\Pinjaman;
 
+use App\Models\PinjamanFakultas;
+use App\Models\PinjamanPustaka;
+use App\Models\PivotPinjaman;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Pengajuan extends Component
 {
+    public $praja, $npp, $pinjaman;
+    public $buttonCreate;
+
+
+    #[On("data-created"), On("failed-creating-data")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+
+
+    public function buatPengajuan()
+    {
+        try {
+            $id = uuid_create(4);
+
+            $data_pustaka = [
+                'PUSTAKA_ID' => $id,
+                'PUSTAKA_PRAJA' => $this->npp,
+                'PUSTAKA_OFFICER' => 1,
+                'PUSTAKA_STATUS' => 'Proses'
+            ];
+
+            PinjamanPustaka::create($data_pustaka);
+
+            $data_fakultas = [
+                'FAKULTAS_ID' => $id,
+                'FAKULTAS_NUMBER' => 'number',
+                'FAKULTAS_PRAJA' => $this->npp,
+                'FAKULTAS_OFFICER' => 1,
+                'FAKULTAS_STATUS' => 'Proses'
+            ];
+
+            PinjamanFakultas::create($data_fakultas);
+
+            $data_pivot = [
+                'PIVOT_ID' => uuid_create(4),
+                'PIVOT_PRAJA' => $this->npp,
+                'PIVOT_PUSTAKA' => $data_pustaka['PUSTAKA_ID'],
+                'PIVOT_FAKULTAS' => $data_fakultas['FAKULTAS_ID'],
+            ];
+
+            PivotPinjaman::create($data_pivot);
+
+            $this->buttonCreate = 'disabled';
+
+            $this->dispatch("data-created", "Pengajuan bebas pinjaman anda berhasil disimpan");
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-creating-data", $th->getMessage());
+        }
+    }
+
+
+
+    public function mount()
+    {
+        $this->npp = explode("@", Auth::user()->email)[0];
+        $this->praja = User::where("id", Auth::user()->id)->first();
+        $this->pinjaman = PivotPinjaman::where('PIVOT_PRAJA', $this->npp)->first();
+        $this->buttonCreate = $this->pinjaman == null ? true : 'disabled';
+    }
+
+
+
     public function render()
     {
         return view('livewire.praja.pinjaman.pengajuan');
--- app/Livewire/Praja/Pinjaman/Resume.php
+++ app/Livewire/Praja/Pinjaman/Resume.php
@@ -2,15 +2,43 @@
 
 namespace App\Livewire\Praja\Pinjaman;
 
+use App\Models\PinjamanFakultas;
 use App\Models\PinjamanPustaka;
 use App\Models\PivotPinjaman;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Resume extends Component
 {
     public $praja, $npp, $dataPinjaman;
-    public $buttonPengajuan;
+    public $buttonResendPustaka = 'hidden', $buttonResendFakultas = 'hidden';
+
+
+
+    #[On("failed-updating-data"), On("data-updated"), On("data-created"), On("failed-creating-data")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+
+
+    public function resendPengajuan($table)
+    {
+        try {
+            if ('fakultas' == $table) {
+                PinjamanFakultas::where('FAKULTAS_PRAJA', $this->npp)->update(['FAKULTAS_STATUS' => 'Proses']);
+            } elseif ('pustaka' == $table) {
+                PinjamanPustaka::where('PUSTAKA_PRAJA', $this->npp)->update(['PUSTAKA_STATUS' => 'Proses']);
+            }
+
+            $this->dispatch("data-updated", "Pengajuan bebas pinjaman anda berhasil diajukan ulang");
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-creating-data", $th->getMessage());
+        }
+    }
+
 
     public function mount()
     {
@@ -20,17 +48,21 @@ class Resume extends Component
         // Nyandak data praja ka server satu praja dumasar kana npp
         $praja = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $this->npp));
         $this->praja = $praja->data[0];
-
-        $this->dataPinjaman = PivotPinjaman::where('PIVOT_PRAJA', $this->npp)->first();
-        $this->dataPinjaman == null ? $this->buttonPengajuan = null : $this->buttonPengajuan = 'hidden';
     }
 
 
 
     public function render()
     {
-        return view('livewire.praja.pinjaman.resume', [
+        $this->dataPinjaman = PivotPinjaman::where('PIVOT_PRAJA', $this->npp)->first();
 
+        if ($this->dataPinjaman != null) {
+            $this->buttonResendPustaka = $this->dataPinjaman->pinjaman_pustaka->PUSTAKA_STATUS != 'Ditolak' ? 'hidden' : null;
+            $this->buttonResendFakultas = $this->dataPinjaman->pinjaman_fakultas->FAKULTAS_STATUS != 'Ditolak' ? 'hidden' : null;
+        }
+
+        return view('livewire.praja.pinjaman.resume', [
+            'data' => $this->dataPinjaman,
             'praja' => $this->praja
         ]);
     }
