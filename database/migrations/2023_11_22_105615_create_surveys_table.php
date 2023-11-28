12fe6b333010cdea7b86cec9d774da8521f14abc
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:35:18 +0700

    UPDATE || Migration survey pustaka add field status, approved and notes

--- database/migrations/2023_11_22_105615_create_surveys_table.php
+++ database/migrations/2023_11_22_105615_create_surveys_table.php
@@ -12,12 +12,16 @@ return new class extends Migration {
     {
         Schema::create('SURVEY', function (Blueprint $table) {
             $table->string("SURVEY_ID", 36)->primary()->comment("Primary Key untuk table survey");
-            $table->text("SURVEY_URL");
+            $table->string('SURVEY_PRAJA', 8)->comment('Foreign Key ke data praja');
             $table->unsignedBigInteger('SURVEY_OFFICER');
+            $table->enum('SURVEY_STATUS', ['Proses', 'Disetujui', 'Ditolak'])->default('Proses');
+            $table->string('SURVEY_APPROVED')->nullable()->comment('Tanggal Approve');
+            $table->text('SURVEY_NOTES')->nullable()->comment('Digunakan untuk memberikan keterangan saat pengajuan di tolak');
             $table->timestamps();
             $table->softDeletes();
 
             $table->foreign('SURVEY_OFFICER')->references('id')->on('users');
+            $table->unsignedBigInteger('SURVEY_OFFICER', 255)->nullable()->change()->comment('Foreign Key ke table user sebagai petugas');
 
         });
     }
