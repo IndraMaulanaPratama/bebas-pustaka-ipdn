2177044916f7271f510675da07a39ad8b920d4cf
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:54:15 +0700

    ADD NEW || Component view surpey pustaka praja

---
+++ resources/views/livewire/praja/survey-praja/pengajuan.blade.php
@@ -0,0 +1,35 @@
+{{-- Stop trying to control. --}}
+
+<div>
+    <ol type="a">
+        <li>
+            <b>Lokasi Verifikasi</b>
+
+            <p>Perpustakaan IPDN Jatinangor</p>
+        </li>
+
+        <li>
+            <b>Petunjuk</b>
+
+            <p>
+                Praja akan mendapatkan <b><i>link Google Form</i></b> yang sudah dibagikan ke ketua kelas, kemudian
+                silahkan lakukan
+                pengisian hingga seluruh item kuisoner terisi. Kami sarankan untuk menggunakan <b><i>laptop</i></b> agar
+                lebih
+                efektif. Kemudian jika sudah dilakukan pengisian survei tersebut silahkan lakukan verifikasi kepada
+                petugas.
+            </p>
+        </li>
+
+        <li>
+            <b>Pengajuan</b>
+
+            <p>
+                silahkan klik tombol <button wire:confirm='Anda yakin akan membuat pengajuan survey perpustakaan?'
+                    wire:click='buatPengajuan' class="btn btn-outline-primary btn-sm" {{ $buttonCreate }}> Buat Pengajuan
+                </button> untuk melakukan pengajuan pemeriksaan survey perpustakaan
+            </p>
+        </li>
+
+    </ol>
+</div>
