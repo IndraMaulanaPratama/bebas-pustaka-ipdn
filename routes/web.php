0b958f104274ba033c260e52664dc610144389df
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 12:02:48 +0700

    UPDATE || Registering new route on web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Page\Admin\Assign;
+use App\Livewire\Page\Admin\BebasPustaka;
 use App\Livewire\Page\Admin\Blank;
 use App\Livewire\Page\Admin\DemoDashboard;
 use App\Livewire\Page\Admin\DonasiElektronik;
@@ -11,8 +12,11 @@ use App\Livewire\Page\Admin\PinjamanFakultas;
 use App\Livewire\Page\Admin\PinjamanPustaka;
 use App\Livewire\Page\Admin\Role;
 use App\Livewire\Page\Admin\Similaritas;
-// use App\Livewire\Page\Praja\Similaritas as PrajaSimilaritas;
+use App\Livewire\Page\Admin\SkripsiFakultas;
+use App\Livewire\Page\Admin\SkripsiPerpustakaan;
+use App\Livewire\Page\Admin\SkripsiSoftCopy;
 use App\Livewire\Page\Admin\Survey;
+use App\Livewire\Page\Admin\UnggahRepository;
 use App\Livewire\Page\Admin\Users;
 use App\Livewire\Page\Dashboard;
 use App\Livewire\Page\Login;
@@ -30,17 +34,15 @@ Route::middleware(['auth', 'access'])->group(function () {
     // --- *** Praja Area *** --- //
     Route::get('/praja/similaritas', App\Livewire\Page\Praja\Similaritas::class)->name('praja-similaritas');
     Route::get('/praja/bebas-pinjaman', \App\Livewire\Page\Praja\Pinjaman::class)->name('praja-pinjaman');
-    Route::get('/praja/donasi', \App\Livewire\Page\Praja\Donasi::class)->name('praja-donasi');
     //
-    Route::get('praja/survey', \App\Livewire\Page\Praja\Blank::class)->name('praja-survey');
-    Route::get('praja/konten-literasi', \App\Livewire\Page\Praja\Blank::class)->name('praja-konten.literasi');
-    Route::get('praja/unggah-repository', \App\Livewire\Page\Praja\Blank::class)->name('praja-unggah.repository');
-    Route::get('praja/hard-copy-skripsi', \App\Livewire\Page\Praja\Blank::class)->name('praja-hard.skripsi');
-    Route::get('praja/soft-copy-skripsi', \App\Livewire\Page\Praja\Blank::class)->name('praja-soft.skripsi');
+    Route::get('/praja/donasi', \App\Livewire\Page\Praja\Donasi::class)->name('praja-donasi');
+    Route::get('praja/survey', \App\Livewire\Page\Praja\SurveyPustaka::class)->name('praja-survey');
+    Route::get('praja/konten-literasi', \App\Livewire\Page\Praja\KontenLiterasi::class)->name('praja-konten.literasi');
+    Route::get('praja/unggah-repository', \App\Livewire\Page\Praja\UnggahRepository::class)->name('praja-unggah.repository');
+    Route::get('praja/copy-skripsi', \App\Livewire\Page\Praja\PengumpulanSkripsi::class)->name('praja-skripsi');
 
 
     // --- *** Officer Area *** --- //
-    Route::get('/dashboard', DemoDashboard::class)->name('admin-dashboard');
     Route::get('/similaritas', Similaritas::class)->name('admin-similaritas'); // TODO: Fitur Print dan export
     Route::get('/bebas-pinjaman-perpustakaan', PinjamanPustaka::class)->name('admin-pinjaman.perpustakaan');
     Route::get('/bebas-pinjaman-fakultas', PinjamanFakultas::class)->name('admin-pinjaman.fakultas'); // TODO:: Fitur Print dan export
@@ -48,11 +50,13 @@ Route::middleware(['auth', 'access'])->group(function () {
     Route::get('/donasi-buku-fakultas', DonasiFakultas::class)->name('admin-donasi.fakultas');
     Route::get('/donasi-buku-elektronik', DonasiElektronik::class)->name('admin-donasi.elektronik');
     Route::get('/survey', Survey::class)->name('admin-survey');
-    Route::get('/konten-literasi', KontenLiterasi::class)->name('admin-konten.literasi'); // TODO:: Buatkan database setting apps
+    Route::get('/konten-literasi', KontenLiterasi::class)->name('admin-konten.literasi');
+    Route::get('/unggah-repository', UnggahRepository::class)->name('admin-unggah.repository');
+    Route::get('/hard-copy-fakultas', SkripsiFakultas::class)->name('admin-hard.fakultas');
+    Route::get('/hard-copy-perpustakaan', SkripsiPerpustakaan::class)->name('admin-hard.perpustakaan');
+    Route::get('/soft-copy-skripsi', SkripsiSoftCopy::class)->name('admin-soft.skripsi');
     //
-    Route::get('/unggah-repository', Blank::class)->name('admin-unggah.repository');
-    Route::get('/hard-copy-skripsi', Blank::class)->name('admin-hard.skripsi');
-    Route::get('/soft-copy-skripsi', Blank::class)->name('admin-soft.skripsi');
+    Route::get('/bebas-pustaka', BebasPustaka::class)->name('admin-pustaka'); // TODO:: Kerjakan ini bos, ayo semangat
 
 
 
