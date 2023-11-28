d7b69eac6e54e242c5f50a511b951fb7caaeade4
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:29:43 +0700

    ADD NEW || Migration Skripsi Fakultas

---
+++ database/migrations/2023_11_24_144910_create_skripsi_fakultas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('SKRIPSI_FAKULTAS', function (Blueprint $table) {
+            $table->string('SKRIPSI_ID', 36)->primary()->comment('Primary Key untuk table hard copy skripsi fakultas');
+            $table->string('SKRIPSI_PRAJA', 8)->comment('Foreign Key ke data praja');
+            $table->unsignedBigInteger('SKRIPSI_OFFICER');
+            $table->enum('SKRIPSI_STATUS', ['Proses', 'Disetujui', 'Ditolak'])->default('Proses');
+            $table->string('SKRIPSI_APPROVED')->nullable()->comment('Tanggal Approve');
+            $table->text('SKRIPSI_NOTES')->nullable()->comment('Digunakan untuk memberikan keterangan saat pengajuan di tolak');
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->foreign('SKRIPSI_OFFICER')->references('id')->on('users');
+            $table->unsignedBigInteger('SKRIPSI_OFFICER', 255)->nullable()->change()->comment('Foreign Key ke table user sebagai petugas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('SKRIPSI_FAKULTAS');
+    }
+};
