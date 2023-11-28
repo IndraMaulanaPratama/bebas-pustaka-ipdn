8eef4a2c136cfe845254d11757b4aac8f04b2051
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:52:59 +0700

    ADD NEW || Component view dashboar praja

---
+++ resources/views/livewire/praja/dashboard/dashboard.blade.php
@@ -0,0 +1,127 @@
+<div class="row g-4">
+
+    <div class="col-lg-8 col-md-8 col-sm-12">
+        <div class="row">
+            <x-admin.components.card.card title='Progres Pengajuan' titleSpan='Bebas Pustaka'>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th class="col-9">Nama Pengajuan</th>
+                            <th class="col-2">Status</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Pemeriksaan Similaritas</td>
+                            <td><span class="badge bg-primary"><i class="bi bi-arrow-clockwise me-1"></i> Proses</span>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>2</td>
+                            <td>Bebas Pinjaman Buku Perpustakaan Pusat</td>
+                            <td><span class="badge bg-success"><i class="bi bi-check2-all me-1"></i> Disetujui</span>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>3</td>
+                            <td>Bebas Pinjaman Buku Perpustakaan Fakultas</td>
+                            <td><span class="badge bg-danger"><i class="bi bi-dash-circle me-1"></i> Ditolak</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>4</td>
+                            <td>Donasi Buku Perpustakaan Pusat</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>5</td>
+                            <td>Donasi Buku Perpustakaan Fakultas</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>6</td>
+                            <td>Bebas Poin Perpustakaan Pusat</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>7</td>
+                            <td>Pengisian Survei</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>8</td>
+                            <td>Konten Literasi</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>9</td>
+                            <td>Unggah data repositori</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>10</td>
+                            <td>Pengumpulan hard copy skripsi di perpustakaan pusat</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>11</td>
+                            <td>Pengumpulan hard copy skripsi di perpustakaan fakultas</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                        <tr>
+                            <td>12</td>
+                            <td>Pengumpulan soft copy skripsi</td>
+                            <td><span class="badge bg-secondary"><i class="bi bi-info-circle me-1"></i> Belum ada
+                                    Pengajuan</span></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+            </x-admin.components.card.card>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="row">
+            <x-admin.components.card.card>
+                b <br />
+                b <br />
+                b <br />
+                b <br />
+                b <br />
+                b <br />
+                b <br />
+            </x-admin.components.card.card>
+
+            <x-admin.components.card.card>
+                c
+            </x-admin.components.card.card>
+
+        </div>
+
+    </div>
+
+</div>
