7c8bffa10e8a4d4712de2aae7261b6ff348b0269
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 12:01:32 +0700

    ADD NEW || Logic for dashboard praja

---
+++ app/Livewire/Praja/Dashboard/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Praja\Dashboard;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+
+
+
+    public function mount()
+    {
+
+    }
+
+
+
+    public function render()
+    {
+        return view('livewire.praja.dashboard.dashboard');
+    }
+}
