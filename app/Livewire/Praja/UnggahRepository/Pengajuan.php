4a478ece479567477cfb930afc1d9341976b1119
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:58:32 +0700

    ADD NEW || Logic for Unggah data repository praja

---
+++ app/Livewire/Praja/UnggahRepository/Pengajuan.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Livewire\Praja\UnggahRepository;
+
+use App\Models\Repository;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class Pengajuan extends Component
+{
+    public $praja, $npp, $data;
+    public $buttonCreate;
+
+
+
+    #[On("data-created"), On("failed-creating-data")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+
+
+    public function buatPengajuan()
+    {
+        try {
+
+            $data = [
+                'REPOSITORY_ID' => uuid_create(4),
+                'REPOSITORY_PRAJA' => $this->npp,
+                'REPOSITORY_OFFICER' => 1,
+                'REPOSITORY_STATUS' => 'Proses'
+            ];
+
+            Repository::create($data);
+
+            $this->buttonCreate = 'disabled';
+
+            $this->dispatch("data-created", "Pengajuan tahap unggah repository anda berhasil disimpan");
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-creating-data", $th->getMessage());
+        }
+    }
+
+
+
+    public function resetForm()
+    {
+        $this->reset();
+    }
+
+
+
+    public function mount()
+    {
+        $this->npp = explode("@", Auth::user()->email)[0];
+        $this->praja = User::where("id", Auth::user()->id)->first();
+        $this->data = Repository::where('REPOSITORY_PRAJA', $this->npp)->first();
+        $this->buttonCreate = $this->data == null ? true : 'disabled';
+    }
+
+
+
+    public function render()
+    {
+        return view('livewire.praja.unggah-repository.pengajuan', [
+            'data' => $this->data,
+        ]);
+    }
+}
---
+++ app/Livewire/Praja/UnggahRepository/Resume.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Livewire\Praja\UnggahRepository;
+
+use App\Models\Repository;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class Resume extends Component
+{
+    public $praja, $npp, $data;
+
+    public $buttonAjukan = 'hidden';
+
+
+
+    #[On("failed-updating-data"), On("data-updated"), On("data-created"), On("failed-creating-data")]
+    public function placeholder()
+    {
+        return view("components.admin.components.spinner.loading");
+    }
+
+
+
+    public function pengajuanUlang()
+    {
+        try {
+
+            Repository::where('REPOSITORY_PRAJA', $this->npp)->update(['REPOSITORY_STATUS' => 'Proses']);
+
+            $this->dispatch("data-updated", "Pengajuan tahap unggah repository anda berhasil diajukan ulang");
+        } catch (\Throwable $th) {
+            $this->dispatch("failed-creating-data", $th->getMessage());
+        }
+    }
+
+
+
+    public function mount()
+    {
+        // Ngadamel data npp dumasar kana email praja
+        $this->npp = explode("@", Auth::user()->email)[0];
+
+        // Nyandak data praja ka server satu praja dumasar kana npp
+        $praja = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $this->npp));
+        $this->praja = $praja->data[0];
+    }
+
+
+    public function render()
+    {
+        $this->data = Repository::where('REPOSITORY_PRAJA', $this->npp)->first();
+
+        if ($this->data != null) {
+            $this->buttonAjukan = $this->data->REPOSITORY_STATUS != 'Ditolak' ? 'hidden' : null;
+        }
+
+        return view('livewire.praja.unggah-repository.resume', [
+            'data' => $this->data,
+            'praja' => $this->praja,
+        ]);
+    }
+}
