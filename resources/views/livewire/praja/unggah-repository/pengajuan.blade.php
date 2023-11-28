0226078970a24aed0593aa5f28fef265c402c448
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:54:31 +0700

    ADD NEW || Component view unggah repository praja

---
+++ resources/views/livewire/praja/unggah-repository/pengajuan.blade.php
@@ -0,0 +1,52 @@
+<div>
+    <ol type="a">
+        <li>
+            <b>Lokasi Verifikasi</b>
+
+            <p>Perpustakaan IPDN Jatinangor</p>
+        </li>
+
+        <li>
+            <b>Petunjuk</b>
+
+            <p>
+                Praja melakukan unggah file ringkasan skripsi pada laman <a href="http://eprints.ipdn.ac.id/"
+                    target="blank">eprints ipdn <sup><i class="bi bi-arrow-up-right-circle-fill"></i></sup></a>. Kemudian
+                silahkan
+                masuk dengan <i>username</i> dan <i>password</i> berdasarkan akun program studi masing-masing. Pastikan
+                <i>file yang
+                    diunggah dalam jenis Pdf dan sudah sesuai template yang ditentukan</i>. Adapun template tersebut
+                dapat Praja
+                unduh pada alamat <a href="https://bit.ly/46RAoeo" target="blank">Buka Tamplate <sup><i
+                            class="bi bi-arrow-up-right-circle-fill"></i></sup></a>. Tutorial tips mudah
+                deposit repository dapat Praja tonton pada
+                laman <a href="https://www.youtube.com/watch?v=yqEX4CzYPAE" target="blank">Youtube Bayu Pambayun <sup><i
+                            class="bi bi-arrow-up-right-circle-fill"></i></sup></a>.
+            </p>
+        </li>
+
+        <li>
+            <b>Pengajuan</b>
+
+            <p>
+                silahkan klik tombol <button wire:confirm='Anda yakin akan membuat pengajuan tahap unggah repository?'
+                    wire:click='buatPengajuan' class="btn btn-outline-primary btn-sm" {{ $buttonCreate }}> Buat
+                    Pengajuan
+                </button> untuk melakukan pengajuan pemeriksaan tahap unggah repository
+            </p>
+        </li>
+
+        <li>
+            <b>Catatan</b>
+
+            <p>
+                Jika status pengajuan verifikasi Praja dilakukan penolakan oleh petugas, maka silahkan Praja lakukan
+                perbaikan pada file ringkasan skripsi sesuai dengan catatan yang diberikan petugas. Kemudian silahkan
+                lakukan upload kembali pada laman Repository IPDN dan selanjutnya lakukan <b>pengajuan ulang</b> pada
+                website
+                Bebas Pustaka menu Repository. Status pengajuan akan berubah menjadi <b>diterima</b> jika file ringkasan
+                skripsi perbaikan sudah sesuai dengan ketentuan berdasarkan verifikasi petugas.
+            </p>
+        </li>
+    </ol>
+</div>
---
+++ resources/views/livewire/praja/unggah-repository/resume.blade.php
@@ -0,0 +1,63 @@
+{{-- Stop trying to control. --}}
+
+<table class="table table-border">
+    <tr>
+        <td class="col-lg-2 col-md-4 col-sm-4 col-xs-5">Nomor Pokok Praja</td>
+        <td>:</td>
+        <td class="col-lg-10">{{ $praja->NPP }}</td>
+    </tr>
+
+    <tr>
+        <td>Nama Lengkap</td>
+        <td>:</td>
+        <td>{{ $praja->NAMA }}</td>
+    </tr>
+
+    <tr>
+        <td>Tempat, Tanggal Lahir</td>
+        <td>:</td>
+        <td>{{ $praja->TEMPAT_LAHIR }}, {{ $praja->TANGGAL_LAHIR }}</td>
+    </tr>
+
+    <tr>
+        <td>Jenis Kelamin</td>
+        <td>:</td>
+        <td>{{ $praja->JENIS_KELAMIN == 'L' ? 'LAKI-LAKI' : 'PEREMPUAN' }}</td>
+    </tr>
+
+    <tr>
+        <td>Agama</td>
+        <td>:</td>
+        <td>{{ $praja->AGAMA }}</td>
+    </tr>
+
+    <tr>
+        <td colspan="3">&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>Status Pengajuan</td>
+        <td>:</td>
+        <td>
+            <div class="row">
+                <div class="col-lg-9 col-md-8 col-sm-12 col-xs-12">
+                    {{ $data->REPOSITORY_STATUS ?? '-' }}
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
+                    <button type="button" class="btn btn-rounded-pill btn-outline-secondary btn-sm" {{ $buttonAjukan }}
+                        wire:click='pengajuanUlang("{{ $data->REPOSITORY_ID ?? null }}")'
+                        wire:confirm='Ajukan kembali perubahan yang sudah dilakukan di unggah repository?'>
+                        <i class="bi bi-arrow-clockwise"></i>Ajukan Ulang
+                    </button>
+                </div>
+            </div>
+        </td>
+    </tr>
+
+    <tr>
+        <td>Catatan Pengajuan</td>
+        <td>:</td>
+        <td>{{ $data->REPOSITORY_NOTES ?? '-' }}</td>
+    </tr>
+</table>
