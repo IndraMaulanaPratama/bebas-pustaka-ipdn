1b5c5a72b2ec5047cbbce8a94dc4dac1dcac3cc8
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-28 11:59:42 +0700

    ADD NEW || Logic for dashboard praja

--- app/Livewire/Page/Dashboard.php
+++ app/Livewire/Page/Dashboard.php
@@ -2,20 +2,36 @@
 
 namespace App\Livewire\Page;
 
+use App\Models\Akses;
 use App\Models\pivotMenu;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
+
+#[Title('Beranda - Bebas Pustaka IPDN')]
 class Dashboard extends Component
 {
-    #[Title('Beranda - Bebas Pustaka IPDN')]
+    public $email, $user;
+
+    public function mount()
+    {
+        $this->email = explode("@", Auth::user()->email)[1];
+    }
+
+
+
 
     public function render()
     {
-        return view('livewire.page.dashboard');
+        $role = $this->email == 'ipdn.ac.id' ? 'admin' : 'praja';
+
+        return view('livewire.page.dashboard', [
+            'role' => $role,
+        ]);
     }
 }
