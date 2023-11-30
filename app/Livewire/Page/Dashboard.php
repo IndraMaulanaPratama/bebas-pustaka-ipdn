3e8d0d4374c49159de425f9d7be8a9a7b945ef6a
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-30 09:42:33 +0700

    UPDATE || Get signal from child component dashboard praja

--- app/Livewire/Page/Dashboard.php
+++ app/Livewire/Page/Dashboard.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -18,12 +19,29 @@ class Dashboard extends Component
 {
     public $email, $user;
 
+
+    #[On("data-rejected"), On("data-updated"), On("data-created")]
+    public function processSuccessfully($message)
+    {
+        session()->reflash();
+        session()->flash('success', $message);
+    }
+
+
+
+    #[On("failed-rejecting-data"), On("failed-updating-data"), On("failed-creating-data")]
+    public function failedProcess($message)
+    {
+        session()->reflash();
+        session()->flash('warning', $message);
+    }
+
+
+
     public function mount()
     {
         $this->email = explode("@", Auth::user()->email)[1];
     }
-
-
 
 
     public function render()
