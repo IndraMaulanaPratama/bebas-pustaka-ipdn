b90c3816e821d77a033d1f43901cf473e1e551bb
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-30 09:43:43 +0700

    UPDATE || Add massage alert on view page dashboard page

--- resources/views/livewire/page/dashboard.blade.php
+++ resources/views/livewire/page/dashboard.blade.php
@@ -1,5 +1,17 @@
 <div class="">
 
+    @if (session('success'))
+        <x-admin.components.alert.success text="{{ session('success') }}" />
+    @endif
+
+    @if (session('warning'))
+        <x-admin.components.alert.warning text="{{ session('warning') }}" />
+    @endif
+
+    @if (session('error'))
+        <x-admin.components.alert.error text="{{ session('error') }}" />
+    @endif
+
     <?php if ('admin' == $role) { ?>
         @livewire('Admin.Dashboard.Dashboard', [], key('dashboard-admin'))
     <?php } elseif ('praja' == $role) { ?>
