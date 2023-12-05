5e61908f6146d59edcd2d91559209f906041b887
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-12-05 10:29:10 +0700

    UPDATE || Add tanggal and Petugas on resume similaritas

--- resources/views/livewire/praja/similaritas/resume.blade.php
+++ resources/views/livewire/praja/similaritas/resume.blade.php
@@ -52,6 +52,22 @@
     </tr>
 
     <tr>
+        <td colspan="3">&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Petugas</td>
+        <td>:</td>
+        <td>{{ $similaritas != null && $similaritas->SIMILARITAS_OFFICER != 1 ? $similaritas->user->name : '-'}}</td>
+    </tr>
+
+    <tr>
+        <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Tanggal Pelayanan</td>
+        <td>:</td>
+        <td>{{ $similaritas->updated_at ?? '-' }}</td>
+    </tr>
+
+    <tr>
         <td class="col-lg-4 col-md-4 col-sm-4 col-xs-5">Status Pengajuan</td>
         <td>:</td>
         <td>
