0396e555a12b04953c28c730f1cb382b880ec37a
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-10 10:04:42 +0700

    ADD NEW || Formulir Survei

---
+++ app/Livewire/Page/Admin/Pengaturan/FormulirSurvei.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire\Page\Admin\Pengaturan;
+
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title("Pengaturan Formulir Survei")]
+class FormulirSurvei extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.admin.pengaturan.formulir-survei');
+    }
+}
---
+++ resources/views/livewire/page/admin/pengaturan/formulir-survei.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Formulir-Survei: To attain knowledge, add things every day; To attain wisdom, subtract things every day.
+</div>
