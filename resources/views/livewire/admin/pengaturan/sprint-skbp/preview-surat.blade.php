946f34dda69f81ccfcc2ece1282302aa1df46bb2
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-01-12 10:28:53 +0700

    UPDATE || KEterangan gagal memuat file sprint skbp on preview

--- resources/views/livewire/admin/pengaturan/sprint-skbp/preview-surat.blade.php
+++ resources/views/livewire/admin/pengaturan/sprint-skbp/preview-surat.blade.php
@@ -4,11 +4,8 @@
 <div class="row justify-content-between">
     <div class="col-12">
         <x-admin.components.card.card title='SURAT PERINTAH TUGAS ' titleSpan='Bebas Pustaka Perpustakaan Pusat'>
-            <object data="{{asset("storage/dokumen/" . $sprint)}}"
-                type="application/pdf" width="100%" height="500px">
-                <p>Unable to display PDF file. <a
-                        href="/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf">Download</a>
-                    instead.</p>
+            <object data="{{ asset('storage/dokumen/' . $sprint) }}" type="application/pdf" width="100%" height="500px">
+                <p>System gagal memuat file sprint skbp</p>
             </object> </x-admin.components.card.card>
     </div>
 </div>
