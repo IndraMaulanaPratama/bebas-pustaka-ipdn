121d9c7dc40483cb7fb44799c49b2d11943571e0
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-02-07 14:58:10 +0700

    UPDATE || Logic create pengajuan praja on survey praja

--- app/Livewire/Praja/SurveyPraja/Pengajuan.php
+++ app/Livewire/Praja/SurveyPraja/Pengajuan.php
@@ -24,13 +24,61 @@ class Pengajuan extends Component
 
 
 
+    public function detailPraja($npp)
+    {
+        $detailPraja = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $npp), true);
+        $this->dataPraja = $detailPraja["data"][0];
+
+        // $tanggalLahir = Carbon::createFromFormat("Y-m-d", $this->dataPraja["TANGGAL_LAHIR"])->format("d M Y");
+        // $jenisKelamin = $this->dataPraja['JENIS_KELAMIN'] == "P" ? "PEREMPUAN" : "LAKI-LAKI";
+
+        // $userPraja = User::where('email', $npp . '@praja.ipdn.ac.id')->first();
+        // $nomorPonsel = $userPraja->nomor_ponsel;
+
+        // $this->prajaNama = $this->dataPraja['NAMA'];
+        // $this->prajaEmail = $this->dataPraja['EMAIL'];
+        // $this->prajaPonsel = $nomorPonsel;
+        // $this->prajaTempatTanggalLahir = $this->dataPraja['TEMPAT_LAHIR'] . ', ' . $tanggalLahir;
+        // $this->prajaJenisKelamin = $jenisKelamin;
+        // $this->prajaProvinsi = $this->dataPraja['PROVINSI'];
+        // $this->prajaKota = $this->dataPraja['KOTA'];
+        // $this->prajaTingkat = $this->dataPraja['TINGKAT'];
+        // $this->prajaAngkatan = $this->dataPraja['ANGKATAN'];
+        // $this->prajaKampus = $this->dataPraja['KAMPUS'];
+        // $this->prajaWisma = $this->dataPraja['WISMA'];
+
+        // $this->prajaPropen = $this->dataPraja['PROGRAM_PENDIDIKAN'];
+        $this->prajaFakultas = $this->dataPraja['FAKULTAS'];
+        // $this->prajaProdi = $this->dataPraja['PROGRAM_STUDI'];
+        // $this->prajaKelas = $this->dataPraja['KELAS'];
+    }
+
+
+
+    public function fakultasPraja($npp)
+    {
+        $this->detailPraja($npp);
+
+        if ($this->prajaFakultas == "POLITIK PEMERINTAHAN") {
+            return "FPP";
+        } elseif ($this->prajaFakultas == "MANAJEMEN PEMERINTAHAN") {
+            return "FMP";
+        } elseif ($this->prajaFakultas == "PERLINDUNGAN MASYARAKAT") {
+            return "FPM";
+        }
+    }
+
+
+
     public function buatPengajuan()
     {
         try {
+            $fakultas = $this->fakultasPraja($this->npp);
 
             $data = [
                 'SURVEY_ID' => uuid_create(4),
                 'SURVEY_PRAJA' => $this->npp,
+                'SURVEY_FAKULTAS' => $fakultas,
                 'SURVEY_OFFICER' => 1,
                 'SURVEY_STATUS' => 'Proses'
             ];
