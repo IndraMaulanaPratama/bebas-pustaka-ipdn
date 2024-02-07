9a567d9f7e3bc5e3ee82cdf0a36e614e230487cc
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-02-07 14:52:12 +0700

    UPDATE || Add new column fakultas on migration konten literasi

--- database/migrations/2023_11_22_151747_create_konten_literasis_table.php
+++ database/migrations/2023_11_22_151747_create_konten_literasis_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->string('KONTEN_ID', 36)->primary()->comment('Primary Key untuk table konten literasi');
             $table->text('KONTEN_URL')->comment('URL menuju konten yang di upload oleh praja');
             $table->string('KONTEN_PRAJA', 8)->comment('Foreign Key ke data praja');
+            $table->string('KONTEN_FAKULTAS', )->comment('Data fakultas praja');
             $table->unsignedBigInteger('KONTEN_OFFICER');
             $table->enum('KONTEN_STATUS', ['Proses', 'Disetujui', 'Ditolak'])->default('Proses');
             $table->string('KONTEN_APPROVED')->nullable()->comment('Tanggal Approve');
