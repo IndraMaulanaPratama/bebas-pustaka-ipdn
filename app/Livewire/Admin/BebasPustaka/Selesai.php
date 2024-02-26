765a2485eda6baab42f0ea05adb75383c6b94054
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-02-26 11:35:11 +0700

    ADD NEW || Logic for Selesai SKBP Page

---
+++ app/Livewire/Admin/BebasPustaka/Selesai.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\BebasPustaka;
+
+use Livewire\Component;
+
+class Selesai extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.bebas-pustaka.selesai');
+    }
+}
