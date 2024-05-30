b16b2d9d62b6dca212641b0c7ce484671baa8f73
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-05-30 13:30:50 +0700

    UPDATE || Add logic update bebas pustaka when approving soft copy skripsi

--- app/Livewire/Admin/SkripsiSoftcopy/Table.php
+++ app/Livewire/Admin/SkripsiSoftcopy/Table.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin\SkripsiSoftcopy;
 
 use App\Models\Akses;
+use App\Models\BebasPustaka;
 use App\Models\Menu;
 use App\Models\PivotSkripsi;
 use App\Models\SkripsiSoftcopy;
@@ -106,12 +107,26 @@ class Table extends Component
     public function approveData($id)
     {
         try {
+            // Mencari data pengajuan soft copy skripsi
+            $skripsi = SkripsiSoftcopy::where('SKRIPSI_ID', $id)->first();
+
+            // Inisialisasi data bebas pustaka
+            $skbp = [
+                'BEBAS_SOFT_COPY' => true,
+            ];
+
+            // Inisialisasi data pengajuan soft copy
             $data = [
                 'SKRIPSI_OFFICER' => Auth::user()->id,
                 'SKRIPSI_STATUS' => "Disetujui",
                 'SKRIPSI_NOTES' => null,
                 'SKRIPSI_APPROVED' => Carbon::now("Asia/Jakarta")->format("Y-m-d H:i:s"),
             ];
+
+            // Proses update data bebas pustaka
+            BebasPustaka::where('BEBAS_PRAJA', $skripsi->SKRIPSI_PRAJA)->update($skbp);
+
+            // Proses update data pengajuan soft copy skripsi
             SkripsiSoftcopy::where("SKRIPSI_ID", $id)->update($data);
 
             $this->dispatch("data-updated", "Pengajuan pengumpulan skripsi berhasil disetujui");
@@ -141,7 +156,7 @@ class Table extends Component
 
         return response()->streamDownload(
             function () use ($pdf) {
-                print($pdf);
+                print ($pdf);
             },
             'PENYERAHAN_SKRIPSI-' . $dataPraja['NAMA'] . '.pdf',
             ["Attachment" => false],
