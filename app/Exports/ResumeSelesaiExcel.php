49357da9ced42a8d9f4bfa2eef09ea4ff2d3e00a
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-09 19:40:17 +0700

    UPDATE || Refactoring template export resume selesai

--- app/Exports/ResumeSelesaiExcel.php
+++ app/Exports/ResumeSelesaiExcel.php
@@ -4,7 +4,6 @@ namespace App\Exports;
 
 use App\Models\BebasPustaka;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
