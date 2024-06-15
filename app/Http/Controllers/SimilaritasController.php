a0343ddfb124a19d5d07c1ebf046f1b386c5fb13
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-15 10:20:57 +0700

    ADD NEW || Similaritas Controller

--- app/Http/Controllers/SimilaritasController.php
+++ app/Http/Controllers/SimilaritasController.php
@@ -2,14 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Similaritas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class SimilaritasController extends Controller
 {
-    public function getPraja($npp)
+    public function index(Request $request)
     {
-        $data = json_decode(file_get_contents(env("APP_PRAJA") . "praja?npp=" . $npp), true);
-        return response()->json($data['data'][0]['EMAIL']);
+
+        try {
+            $similaritas = Similaritas::
+                when(
+                    // <!-- Pilari data pengajuan dumasar kana status
+                    $request->has('status'),
+                    function ($query, $status) {
+                        return $query->where("SIMILARITAS_STATUS", $status);
+                    }
+                )
+                ->when(
+                    // <!-- Pilari data pengajuan dumasar kana fakultas
+                    $request->has('fakultas'),
+                    function ($query, $fakultas) {
+                        return $query->where("SIMILARITAS_NUMBER", "LIKE", '%' . $fakultas . '%');
+                    }
+                )
+                ->when(
+                    // <!-- Pilari data pengajuan dumasar kana npp
+                    $request->has('npp'),
+                    function ($query, $npp) {
+                        return $query->where("SIMILARITAS_PRAJA", "LIKE", $npp . "%");
+                    }
+                )
+                ->when(
+                    // <!-- Pilari data pengajuan dumasar kana npp
+                    $request->has('angkatan'),
+                    function ($query, $angkatan) {
+                        return $query->where("SIMILARITAS_PRAJA", "LIKE", $angkatan . "%");
+                    }
+                )
+                ->latest()
+                ->paginate();
+
+            return response()->json([
+                'message' => 'Data similaritas berhasil dimuat',
+                'data' => $similaritas
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Data similaritas gagal dimuat',
+                'error' => $th->getMessage()
+            ], 500);
+        }
+
+
     }
 }
