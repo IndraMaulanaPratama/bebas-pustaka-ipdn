1a41bd93d30cd7a8760671ff4087b16eb1ea5197
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-06-15 10:22:23 +0700

    UPDATE || Add new api route for auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SimilaritasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/**
+ * Route kanggo authentication
+ */
+Route::post('login', [AuthController::class, 'login']);
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::post('refresh-token', [AuthController::class, 'refreshToken']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+/** Route kanggo Similaritas */
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::get('/similaritas/search', [SimilaritasController::class, 'index']);
 });
