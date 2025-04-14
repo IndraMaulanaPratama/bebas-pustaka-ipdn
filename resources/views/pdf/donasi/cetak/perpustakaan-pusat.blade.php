b5fd17178bb95700047f523d3f46aafaada12a4c
IndraMaulanaPratama <maulana.indra808@gmail.com>
2025-04-14 17:09:32 +0700

    UPDATE || Removing papper number on pdf file donasi cetak perpustakaan pusat

--- resources/views/pdf/donasi/cetak/perpustakaan-pusat.blade.php
+++ resources/views/pdf/donasi/cetak/perpustakaan-pusat.blade.php
@@ -84,7 +84,7 @@
                 <h3>BUKTI DONASI KOLEKSI CETAK</h3>
                 <h4>PERPUSTAKAAN PUSAT IPDN JATINANGOR</h4>
                 {{-- <h4>NOMOR: 000.5.6.2/BDKC-dummy-nomor/IPDN.18.4/2025</h4> --}}
-                <h4>NOMOR: {{ $donasi->PUSTAKA_NUMBER }} </h4>
+                {{-- <h4>NOMOR: {{ $donasi->PUSTAKA_NUMBER }} </h4> --}}
             </center>
         </div>
 
