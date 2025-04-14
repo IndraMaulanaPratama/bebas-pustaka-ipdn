2ee549d22eaec14a57e94df6a522728ec477c3a8
IndraMaulanaPratama <maulana.indra808@gmail.com>
2025-04-14 13:09:47 +0700

    UPDATE || Change route name dashboard

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', Dashboard::class)->middleware('auth')->name('/');
+Route::get('/', Dashboard::class)->middleware('auth')->name('dashboard');
 
 
 
