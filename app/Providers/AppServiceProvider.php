e71b71dd1ab55fea65ba6d2e9af416cdda128f3f
IndraMaulanaPratama <maulana.indra808@gmail.com>
2025-08-23 07:43:25 +0700

    ADD NEW || Custom logic for split domain socialite google auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Socialite\Facades\Socialite;
+use App\Services\Google\CustomGoogleProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +20,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Extend Socialite dengan multiple Google drivers
+        Socialite::extend('google_pegawai', function ($app) {
+            $config = $app['config']['services.google_pegawai'];
+            return Socialite::buildProvider(CustomGoogleProvider::class, $config);
+        });
+
+        Socialite::extend('google_praja', function ($app) {
+            $config = $app['config']['services.google_praja'];
+            return Socialite::buildProvider(CustomGoogleProvider::class, $config);
+        });
     }
 }
