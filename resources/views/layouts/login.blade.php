d0730ba72d6e0bcc38c545340b483649a0fbef40
IndraMaulanaPratama <maulana.indra808@gmail.com>
2025-08-23 10:36:08 +0700

    UPDATE || Fixing bug style on login page

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -12,12 +12,10 @@
     <!-- Favicons -->
     <link href="./assets/admin/img/favicon.png" rel="icon">
     <link href="./assets/admin/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!-- Google Fonts -->
-    {{-- <link href="https://fonts.gstatic.com" rel="preconnect"> --}}
-    {{-- <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet"> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Vendor CSS Files -->
     <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
